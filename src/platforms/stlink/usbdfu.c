7c2e994381338d837fb928f8ef8019315e1baec9
Gareth McMullin <gareth@blacksphere.co.nz>
2013-03-12 19:04:10 +1300

    Split out platform specific bootloader parts to separate files.

---
+++ src/platforms/stlink/usbdfu.c
@@ -0,0 +1,136 @@
+/*
+ * This file is part of the Black Magic Debug project.
+ *
+ * Copyright (C) 2013 Gareth McMullin <gareth@blacksphere.co.nz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/scb.h>
+
+#include "usbdfu.h"
+
+static uint8_t rev;
+static uint16_t led_idle_run;
+static uint32_t led2_state = 0;
+
+static int stlink_test_nrst(void)
+{
+	/* Test if JRST/NRST is pulled down*/
+	int i;
+	uint16_t nrst;
+	uint16_t pin;
+
+	/* First, get Board revision by pulling PC13/14 up. Read
+	 *  11 for ST-Link V1, e.g. on VL Discovery, tag as rev 0
+	 *  10 for ST-Link V2, e.g. on F4 Discovery, tag as rev 1
+	 */
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
+	gpio_set_mode(GPIOC, GPIO_MODE_INPUT,
+			GPIO_CNF_INPUT_PULL_UPDOWN, GPIO14 | GPIO13);
+	gpio_set(GPIOC, GPIO14 | GPIO13);
+	for (i = 0; i < 100; i++)
+		rev = (~(gpio_get(GPIOC, GPIO14 | GPIO13)) >> 13) & 3;
+
+	switch (rev) {
+	case 0:
+		pin = GPIO1;
+		led_idle_run = GPIO8;
+		break;
+	default:
+		pin = GPIO0;
+		led_idle_run = GPIO9;
+	}
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+			GPIO_CNF_OUTPUT_PUSHPULL, led_idle_run);
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPBEN);
+	gpio_set_mode(GPIOB, GPIO_MODE_INPUT,
+			GPIO_CNF_INPUT_PULL_UPDOWN, pin);
+	gpio_set(GPIOB, pin);
+	for (i = 0; i < 100; i++)
+		nrst = gpio_get(GPIOB, pin);
+	return (nrst) ? 1 : 0;
+}
+
+void dfu_detach(void)
+{
+	/* Disconnect USB cable by resetting USB Device
+	   and pulling USB_DP low*/
+	rcc_peripheral_reset(&RCC_APB1RSTR, RCC_APB1ENR_USBEN);
+	rcc_peripheral_clear_reset(&RCC_APB1RSTR, RCC_APB1ENR_USBEN);
+	rcc_peripheral_enable_clock(&RCC_APB1ENR, RCC_APB1ENR_USBEN);
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPAEN);
+	gpio_clear(GPIOA, GPIO12);
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+		GPIO_CNF_OUTPUT_OPENDRAIN, GPIO12);
+	scb_reset_system();
+}
+
+int main(void)
+{
+	/* Check the force bootloader pin*/
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPAEN);
+	/* Check value of GPIOA1 configuration. This pin is unconnected on
+	 * STLink V1 and V2. If we have a value other than the reset value (0x4),
+	 * we have a warm start and request Bootloader entry
+	 */
+	if(((GPIOA_CRL & 0x40) == 0x40) && stlink_test_nrst())
+		dfu_jump_app_if_valid();
+
+	dfu_protect_enable();
+
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+	systick_set_clocksource(STK_CTRL_CLKSOURCE_AHB_DIV8);
+	systick_set_reload(900000);
+
+        /* Handle USB disconnect/connect */
+	/* Just in case: Disconnect USB cable by resetting USB Device
+         * and pulling USB_DP low
+         * Device will reconnect automatically as Pull-Up is hard wired*/
+	rcc_peripheral_reset(&RCC_APB1RSTR, RCC_APB1ENR_USBEN);
+	rcc_peripheral_clear_reset(&RCC_APB1RSTR, RCC_APB1ENR_USBEN);
+	rcc_peripheral_enable_clock(&RCC_APB1ENR, RCC_APB1ENR_USBEN);
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPAEN);
+	gpio_clear(GPIOA, GPIO12);
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+		GPIO_CNF_OUTPUT_OPENDRAIN, GPIO12);
+
+	systick_interrupt_enable();
+	systick_counter_enable();
+
+	dfu_init(&stm32f103_usb_driver);
+
+	dfu_main();
+}
+
+void sys_tick_handler(void)
+{
+	if (rev == 0) {
+		gpio_toggle(GPIOA, led_idle_run);
+	} else {
+		if (led2_state & 1) {
+			gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+				GPIO_CNF_OUTPUT_PUSHPULL, led_idle_run);
+		} else {
+			gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
+				GPIO_CNF_INPUT_ANALOG, led_idle_run);
+		}
+		led2_state++;
+	}
+}
+
