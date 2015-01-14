a73822f469b8b6f377d3b4e5c0aad511520d8f3f
Benjamin Vernoux <bvernoux@gmail.com>
2015-01-14 11:05:02 +0100

    Update platform.c

--- src/platforms/hydrabus/platform.c
+++ src/platforms/hydrabus/platform.c
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 2011  Black Sphere Technologies Ltd.
  * Written by Gareth McMullin <gareth@blacksphere.co.nz>
+ * Copyright (C) 2015  Benjamin Vernoux <bvernoux@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
