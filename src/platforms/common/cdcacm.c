2365bbe010364e61b35d4ae0b38696d119c65b97
Antti Nykänen <aon@umetronics.com>
2015-09-30 15:34:57 +0300

    TI TM4C/LM4F cdc-acm fixes

--- src/platforms/common/cdcacm.c
+++ src/platforms/common/cdcacm.c
@@ -498,7 +498,7 @@ static void cdcacm_set_config(usbd_device *dev, uint16_t wValue)
 	configured = wValue;
 
 	/* GDB interface */
-#ifdef STM32F4
+#if defined(STM32F4) || defined(LM4F)
 	usbd_ep_setup(dev, 0x01, USB_ENDPOINT_ATTR_BULK,
 	              CDCACM_PACKET_SIZE, gdb_usb_out_cb);
 #else
