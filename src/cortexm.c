4addec0a21513753008494c24f6fcc5b82d2c88b
David Lawrence <dlaw@markforged.com>
2016-06-09 18:20:07 -0400

    lpc15xx support

--- src/cortexm.c
+++ src/cortexm.c
@@ -282,6 +282,7 @@ bool cortexm_probe(ADIv5_AP_t *ap)
 	PROBE(stm32l0_probe);   /* STM32L0xx & STM32L1xx */
 	PROBE(stm32l4_probe);
 	PROBE(lpc11xx_probe);
+	PROBE(lpc15xx_probe);
 	PROBE(lpc43xx_probe);
 	PROBE(sam3x_probe);
 	PROBE(nrf51_probe);
