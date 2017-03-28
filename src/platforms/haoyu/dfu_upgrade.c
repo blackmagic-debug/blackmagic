da46e4ac51a25d1b226e4a221c5f06693fc479ed
Dave Marples <dave@marples.net>
2017-03-28 17:18:08 +0100

    Addition of Haoyu board support

---
+++ src/platforms/haoyu/dfu_upgrade.c
@@ -0,0 +1,96 @@
+/*
+ * This file is part of the Black Magic Debug project.
+ *
+ * Copyright (C) 2013 Gareth McMullin <gareth@blacksphere.co.nz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/scb.h>
+
+#include "usbdfu.h"
+#include "platform.h"
+
+uint32_t app_address = 0x08000000;
+
+void dfu_detach(void)
+{
+	/* Disconnect USB cable by resetting USB Device and pulling USB_DP low */
+	rcc_periph_reset_pulse(RST_USB);
+	rcc_periph_clock_enable(RCC_USB);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_clear(GPIOA, GPIO12);
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+		GPIO_CNF_OUTPUT_OPENDRAIN, GPIO12);
+
+#ifdef TRST_PORT
+	/* Pull T_NRST low */
+	rcc_periph_clock_enable(RCC_GPIOB);
+	gpio_set_mode(TRST_PORT, GPIO_MODE_OUTPUT_2_MHZ,
+		GPIO_CNF_OUTPUT_OPENDRAIN, TRST_PIN);
+	gpio_clear(TRST_PORT, TRST_PIN);
+#endif
+	SCB_VTOR = 0;
+	scb_reset_core();
+}
+
+
+int main(void)
+{
+
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+
+    rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_periph_clock_enable(RCC_GPIOB);
+    rcc_periph_clock_enable(RCC_GPIOC);
+    gpio_set_mode(LED_PORT, GPIO_MODE_OUTPUT_2_MHZ,
+            GPIO_CNF_OUTPUT_PUSHPULL, LED_IDLE_RUN);
+    gpio_set(USB_PU_PORT,USB_PU_PIN);
+
+	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
+	systick_set_reload(900000);
+
+	dfu_protect(UPD_MODE);
+
+	/* Handle USB disconnect/connect */
+	/* Just in case: Disconnect USB cable by resetting USB Device
+	 * and pulling USB_DP low
+	 * Device will reconnect automatically as Pull-Up is hard wired*/
+	rcc_periph_reset_pulse(RST_USB);
+	rcc_periph_clock_enable(RCC_USB);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_clear(GPIOA, GPIO12);
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+		GPIO_CNF_OUTPUT_OPENDRAIN, GPIO12);
+
+	systick_interrupt_enable();
+	systick_counter_enable();
+
+	dfu_init(&stm32f103_usb_driver, UPD_MODE);
+
+	dfu_main();
+}
+
+void dfu_event(void)
+{
+}
+
+void sys_tick_handler(void)
+{
+		gpio_toggle(LED_PORT, LED_IDLE_RUN);
+}
---
+++ src/platforms/haoyu/platform.h
@@ -0,0 +1,152 @@
+/*
+ * This file is part of the Black Magic Debug project.
+ *
+ * Copyright (C) 2011  Black Sphere Technologies Ltd.
+ * Written by Gareth McMullin <gareth@blacksphere.co.nz>
+ * Patching for Haoyu board by Dave Marples <dave@marples.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* This file implements the platform specific functions for the Hapyu
+ * implementation.
+ */
+#ifndef __PLATFORM_H
+#define __PLATFORM_H
+
+#include "gpio.h"
+#include "timing.h"
+#include "timing_stm32.h"
+#include "version.h"
+
+#include <libopencm3/cm3/common.h>
+#include <libopencm3/stm32/f1/memorymap.h>
+#include <libopencm3/usb/usbd.h>
+
+#define BOARD_IDENT       "Black Magic Probe (Haoyu), (Firmware " FIRMWARE_VERSION ")"
+#define BOARD_IDENT_DFU   "Black Magic for Haoyu, (Firmware " FIRMWARE_VERSION ")"
+#define BOARD_IDENT_UPD   "Black Magic (DFU Upgrade) for Haoyu, (Firmware " FIRMWARE_VERSION ")"
+#define DFU_IDENT         "Black Magic Firmware Upgrade (Haoyu) (Firmware " FIRMWARE_VERSION ")"
+#define DFU_IFACE_STRING  "@Internal Flash   /0x08000000/8*001Ka,56*001Kg"
+#define UPD_IFACE_STRING  "@Internal Flash   /0x08000000/8*001Kg"
+
+/* Important pin mappings for STM32 Haoyu implementation:
+ *
+ * LED0 = 	PB12	(Yellow LED : Running)
+ *
+ * These are fixed by the implementation of the hardware
+ * SRST_OUT = 	PA2
+ * TDI = 	PB0
+ * TMS = 	PA4 (input for SWDP)
+ * TCK = 	PA3 
+ * TDO = 	PA6 (input)
+ * nSRST = 	PA2 (input)
+ *
+ * Serial port;
+ * PB6
+ * PB7
+ *
+ * USB cable pull-up: PA9
+ */
+#define PLATFORM_HAS_TRACESWO 1
+/* Hardware definitions... */
+#define TRST_PORT GPIOB
+#define TRST_PIN  GPIO1
+
+#define TDI_PORT	GPIOB
+#define TMS_PORT	GPIOA
+#define TCK_PORT	GPIOA
+#define TDO_PORT	GPIOA
+#define TDI_PIN		GPIO0
+#define TMS_PIN		GPIO4
+#define TCK_PIN		GPIO3
+#define TDO_PIN		GPIO6
+
+#define SWDIO_PORT 	TMS_PORT
+#define SWCLK_PORT 	TCK_PORT
+#define SWDIO_PIN	TMS_PIN
+#define SWCLK_PIN	TCK_PIN
+
+#define SRST_PORT	GPIOA
+#define SRST_PIN	GPIO2
+
+#define USB_PU_PORT     GPIOA
+#define USB_PU_PIN      GPIO9
+
+#define LED_PORT	GPIOB
+#define LED_IDLE_RUN    GPIO12
+
+/* Use PC14 for a "dummy" uart led. So we can observere at least with scope*/
+#define LED_PORT_UART	GPIOC
+#define LED_UART	GPIO14
+
+#define TMS_SET_MODE() \
+	gpio_set_mode(TMS_PORT, GPIO_MODE_OUTPUT_50_MHZ, \
+	              GPIO_CNF_OUTPUT_PUSHPULL, TMS_PIN);
+#define SWDIO_MODE_FLOAT() \
+	gpio_set_mode(SWDIO_PORT, GPIO_MODE_INPUT, \
+	              GPIO_CNF_INPUT_FLOAT, SWDIO_PIN);
+#define SWDIO_MODE_DRIVE() \
+	gpio_set_mode(SWDIO_PORT, GPIO_MODE_OUTPUT_50_MHZ, \
+	              GPIO_CNF_OUTPUT_PUSHPULL, SWDIO_PIN);
+
+#define UART_PIN_SETUP() \
+	gpio_set_mode(USBUSART_PORT, GPIO_MODE_OUTPUT_2_MHZ, \
+	              GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, USBUSART_TX_PIN);
+
+#define USB_DRIVER      stm32f103_usb_driver
+#define USB_IRQ	        NVIC_USB_LP_CAN_RX0_IRQ
+#define USB_ISR	        usb_lp_can_rx0_isr
+/* Interrupt priorities.  Low numbers are high priority.
+ * For now USARTq preempts USB which may spin while buffer is drained.
+ * TIM3 is used for traceswo capture and must be highest priority.
+ */
+#define IRQ_PRI_USB		(2 << 4)
+#define IRQ_PRI_USBUSART	(1 << 4)
+#define IRQ_PRI_USBUSART_TIM	(3 << 4)
+#define IRQ_PRI_USB_VBUS	(14 << 4)
+#define IRQ_PRI_TIM3		(0 << 4)
+
+#define USBUSART USART1
+#define USBUSART_CR1 USART1_CR1
+#define USBUSART_IRQ NVIC_USART1_IRQ
+#define USBUSART_CLK RCC_USART1
+#define USBUSART_PORT GPIOB
+#define USBUSART_TX_PIN GPIO6
+#define USBUSART_ISR usart1_isr
+#define USBUSART_TIM TIM4
+#define USBUSART_TIM_CLK_EN() rcc_periph_clock_enable(RCC_TIM4)
+#define USBUSART_TIM_IRQ NVIC_TIM4_IRQ
+#define USBUSART_TIM_ISR tim4_isr
+
+#define TRACE_TIM_CLK_EN() rcc_periph_clock_enable(RCC_TIM3)
+#define TRACE_TIM   TIM3
+#define TRACE_IRQ   NVIC_TIM3_IRQ
+#define TRACE_ISR   tim3_isr
+#define IRQ_PRI_TRACE           (0 << 4)
+
+#define DEBUG(...)
+
+#define SET_RUN_STATE(state)	{running_status = (state);}
+#define SET_IDLE_STATE(state)	{gpio_set_val(LED_PORT, LED_IDLE_RUN, state);}
+#define SET_ERROR_STATE(x)
+
+/* Use newlib provided integer only stdio functions */
+#define sscanf siscanf
+#define sprintf siprintf
+#define vasprintf vasiprintf
+#define snprintf sniprintf
+
+#endif
+
