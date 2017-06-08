798d8835804621ff3fe0fb2e6522cf0db8070942
Dave Marples <dave@marples.net>
2017-06-08 10:24:21 +0100

    Cludges to memory size for F103x8 devices to make them look like F103xC devices (with 128K)

--- src/platforms/haoyu/platform.h
+++ src/platforms/haoyu/platform.h
@@ -38,7 +38,6 @@
 #define BOARD_IDENT_DFU   "Black Magic for Haoyu, (Firmware " FIRMWARE_VERSION ")"
 #define BOARD_IDENT_UPD   "Black Magic (DFU Upgrade) for Haoyu, (Firmware " FIRMWARE_VERSION ")"
 #define DFU_IDENT         "Black Magic Firmware Upgrade (Haoyu) (Firmware " FIRMWARE_VERSION ")"
-#define DFU_IFACE_STRING  "@Internal Flash   /0x08000000/8*001Ka,56*001Kg"
 #define UPD_IFACE_STRING  "@Internal Flash   /0x08000000/8*001Kg"
 
 /* Important pin mappings for STM32 Haoyu implementation:
