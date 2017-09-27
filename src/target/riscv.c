f10641978f85aa4104958b506d01ae775fceb6b1
Gareth McMullin <gareth@blacksphere.co.nz>
2017-09-27 14:23:23 +1300

    riscv: Report architecture as rv32

--- src/target/riscv.c
+++ src/target/riscv.c
@@ -52,6 +52,13 @@
 #define RISCV_DPC      0x7b1
 #define RISCV_DSCRATCH 0x7b2
 
+/* GDB register map / target description */
+static const char tdesc_rv32[] =
+"<?xml version=\"1.0\"?>"
+"<target>"
+"  <architecture>riscv:rv32</architecture>"
+"</target>";
+
 struct riscv_dtm {
 	uint8_t dtm_index;
 	uint8_t version; /* As read from dmtcontrol */
@@ -456,4 +463,5 @@ void riscv_jtag_handler(jtag_dev_t *jd)
 	t->halt_poll = riscv_halt_poll;
 	t->halt_resume = riscv_halt_resume;
 	t->regs_size = 33 * 4;
+	t->tdesc = tdesc_rv32;
 }
