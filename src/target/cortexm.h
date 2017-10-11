c53a12bfd1fc9ce4d41dd4bd3ceb87cae917c96f
Gareth McMullin <gareth@blacksphere.co.nz>
2017-10-12 09:26:01 +1300

    cortexm: Better cache support for Cortex-M7
    - On probe, read CTR for cache presence and minimum line length
    - Make D-Cache clean a function
    - Clean before memory reads
    - Clean and invalidate before memory writes
    - Flush all I-Cache before resume

--- src/target/cortexm.h
+++ src/target/cortexm.h
@@ -37,7 +37,15 @@
 #define CORTEXM_DCRDR		(CORTEXM_SCS_BASE + 0xDF8)
 #define CORTEXM_DEMCR		(CORTEXM_SCS_BASE + 0xDFC)
 
-/* Data cache clean and invalidate by address to the PoC=Point of Coherency */
+/* Cache identification */
+#define CORTEXM_CLIDR		(CORTEXM_SCS_BASE + 0xD78)
+#define CORTEXM_CTR		(CORTEXM_SCS_BASE + 0xD7C)
+#define CORTEXM_CCSIDR		(CORTEXM_SCS_BASE + 0xD80)
+#define CORTEXM_CSSELR		(CORTEXM_SCS_BASE + 0xD84)
+
+/* Cache maintenance operations */
+#define CORTEXM_ICIALLU		(CORTEXM_SCS_BASE + 0xF50)
+#define CORTEXM_DCCMVAC		(CORTEXM_SCS_BASE + 0xF68)
 #define CORTEXM_DCCIMVAC	(CORTEXM_SCS_BASE + 0xF70)
 
 #define CORTEXM_FPB_BASE	(CORTEXM_PPB_BASE + 0x2000)
