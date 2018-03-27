6127a6431ee771125b78d050432253199661be18
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2018-03-27 12:48:32 +0200

    stlink: Check nRST line level when setting SRST.
    Problem: On some boards flashing hanged.
    Cause: Releasing SRST caused a slow rise of nRST and flashing
    started while the target still was in reset.
    Attention: platform_delay(ms) only resolved 0.1 s.
    Nucleo-P boards have SRST unconnected to target nRST by default.

--- src/platforms/stlink/platform.c
+++ src/platforms/stlink/platform.c
@@ -90,10 +90,21 @@ void platform_init(void)
 
 void platform_srst_set_val(bool assert)
 {
-	if (assert)
-		gpio_clear(SRST_PORT, srst_pin);
-	else
-		gpio_set(SRST_PORT, srst_pin);
+	uint32_t crl = GPIOB_CRL;
+	uint32_t shift = (srst_pin == GPIO0) ? 0 : 4;
+	uint32_t mask = 0xf << shift;
+	crl &= ~mask;
+	if (assert) {
+		/* Set SRST as Open-Drain, 50 Mhz, low.*/
+		GPIOB_BRR = srst_pin;
+		GPIOB_CRL = crl | (7 << shift);
+	} else {
+		/* Set SRST as input, pull-up.
+		 * SRST might be unconnected, e.g on Nucleo-P!*/
+		GPIOB_CRL = crl | (8 << shift);
+		GPIOB_BSRR = srst_pin;
+	}
+	while (gpio_get(SRST_PORT, srst_pin) == assert) {};
 }
 
 bool platform_srst_get_val()
