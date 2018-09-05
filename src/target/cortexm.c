f39701c4c8977dfcaa1548573fa8aae7777d2891
Rik van der Heijden <mail@rikvanderheijden.com>
2018-09-05 17:40:02 +0200

    Move the LPC17xx probe function down since it performs an IAP call which can hang when performed on other devices than a LPC17xx

--- src/target/cortexm.c
+++ src/target/cortexm.c
@@ -335,7 +335,6 @@ bool cortexm_probe(ADIv5_AP_t *ap)
 	PROBE(stm32l4_probe);
 	PROBE(lpc11xx_probe);
 	PROBE(lpc15xx_probe);
-	PROBE(lpc17xx_probe);
 	PROBE(lpc43xx_probe);
 	PROBE(sam3x_probe);
 	PROBE(sam4l_probe);
@@ -345,6 +344,7 @@ bool cortexm_probe(ADIv5_AP_t *ap)
 	PROBE(kinetis_probe);
 	PROBE(efm32_probe);
 	PROBE(msp432_probe);
+	PROBE(lpc17xx_probe);
 #undef PROBE
 
 	return true;
