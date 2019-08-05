ef372d01617f599afa17d7809389aac90a1f6066
Sylvain Munaut <tnt@246tNt.com>
2019-08-05 15:24:16 +0200

    build: Use a separate NO_LIBOPENCM3 flag to disable libopencm3
    This prepares to allow other architectures than pc
    that don't use libopencm3.
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- src/include/gdb_if.h
+++ src/include/gdb_if.h
@@ -21,7 +21,7 @@
 #ifndef __GDB_IF_H
 #define __GDB_IF_H
 
-#if !defined(PC_HOSTED)
+#if !defined(NO_LIBOPENCM3)
 #include <libopencm3/usb/usbd.h>
 void gdb_usb_out_cb(usbd_device *dev, uint8_t ep);
 #endif
