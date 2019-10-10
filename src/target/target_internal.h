26216beaab9eb310a376f159160c2f2f58735366
Ken Healy <kh90909@users.noreply.github.com>
2019-10-10 00:38:36 -0400

    Microchip SAM D51 / E5x support
    Adds a target driver for Microchip SAM D51 / E5x family.
    
    Tested on SAMD51G19A and SAMD51J19A. According to the datasheet, the
    D51 / E5x family share the same core feature set, differing only in the
    addition of CAN (E51) or ethernet controllers (E53/54). All members of
    the family should be equivalent from a debug and programming perspective.

--- src/target/target_internal.h
+++ src/target/target_internal.h
@@ -178,6 +178,7 @@ bool sam3x_probe(target *t);
 bool sam4l_probe(target *t);
 bool nrf51_probe(target *t);
 bool samd_probe(target *t);
+bool samx5x_probe(target *t);
 bool kinetis_probe(target *t);
 bool efm32_probe(target *t);
 bool msp432_probe(target *t);
