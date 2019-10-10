a71667327433caa70bda095e0ba9ea4b093c252c
Ken Healy <kh90909@users.noreply.github.com>
2019-10-10 00:38:36 -0400

    Microchip SAM D51 / E5x support
    Adds a target driver for Microchip SAM D51 / E5x family.
    
    Tested on SAMD51G19A and SAMD51J19A. According to the datasheet, the
    D51 / E5x family share the same core feature set, differing only in the
    addition of CAN (E51) or ethernet controllers (E53/54). All members of
    the family should be equivalent from a debug and programming perspective.

--- src/target/cortexm.c
+++ src/target/cortexm.c
@@ -370,6 +370,7 @@ bool cortexm_probe(ADIv5_AP_t *ap, bool forced)
 	PROBE(sam4l_probe);
 	PROBE(nrf51_probe);
 	PROBE(samd_probe);
+	PROBE(samx5x_probe);
 	PROBE(lmi_probe);
 	PROBE(kinetis_probe);
 	PROBE(efm32_probe);
