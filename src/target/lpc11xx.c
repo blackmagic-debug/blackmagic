f9f928e9d6ed9c1633480698cd1471bbab7fbd7f
Thomas Bénéteau <thomas@bitwise.me>
2020-06-02 14:44:03 +1000

    Add support for LPC8N04

--- src/target/lpc11xx.c
+++ src/target/lpc11xx.c
@@ -129,6 +129,12 @@ lpc11xx_probe(target *t)
 		target_add_ram(t, 0x10000000, 0x2000);
 		lpc11xx_add_flash(t, 0x00000000, 0x8000, 0x1000);
 		return true;
+	case 0x00008A04:  /* LPC8N04 (see UM11074 Rev.1.3 section 4.5.19) */
+		t->driver = "LPC8N04";
+		target_add_ram(t, 0x10000000, 0x2000);
+		lpc11xx_add_flash(t, 0x00000000, 0x8000, 0x400);
+		target_add_commands(t, lpc11xx_cmd_list, "LPC8N04");
+		return true;
 	}
 	if (idcode) {
 		DEBUG("LPC11xx: Unknown IDCODE 0x%08" PRIx32 "\n", idcode);
