blackmagic-debug/blackmagic

commit 72387164eb52351e5c9c6afa1e9d7be1a097fa83

hosted/stlinkv2.c: Handle AP_WAIT error when switching to SWD

src/platforms/hosted/stlinkv2.c
@@ -1089,7 +1089,7 @@ int stlink_enter_debug_swd(bmp_info_t *info, ADIv5_DP_t *dp)
 					  STLINK_DEBUG_ENTER_SWD_NO_RESET};
 	uint8_t data[2];
 	DEBUG_INFO("Enter SWD\n");
-	send_recv(info->usb_link, cmd, 16, data, 2);
+	stlink_send_recv_retry(cmd, 16, data, 2);
 	if (stlink_usb_error_check(data, true))
 		return -1;
 	dp->idcode = stlink_read_coreid();

commit 0bc59640fe3e4f7aa3ac600089e72c1275fc6271

hosted/stlinkv2.c: Fix bug introduced with #703

src/platforms/hosted/stlinkv2.c
@@ -689,7 +689,7 @@ static uint32_t stlink_read_coreid(void)
 	uint8_t cmd[16] = {STLINK_DEBUG_COMMAND, STLINK_DEBUG_APIV2_READ_IDCODES};
 	uint8_t data[12];
 	send_recv(info.usb_link, cmd, 16, data, 12);
-	uint32_t id =  data[4] | data[5] << 8 | data[6] << 16 | data[6] << 24;
+	uint32_t id =  data[4] | data[5] << 8 | data[6] << 16 | data[7] << 24;
 	DEBUG_INFO("Read Core ID: 0x%08" PRIx32 "\n", id);
 	return id;
 }