f98b7274b42d0debb362a9c569b234befaa152bb
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2020-12-20 13:30:49 +0100

    hosted/stlinkv3: Detect STlinkV3 in bootloader mode.

--- src/platforms/hosted/platform.h
+++ src/platforms/hosted/platform.h
@@ -14,6 +14,19 @@ void platform_buffer_flush(void);
 #define PRODUCT_ID_BMP_BL        0x6017
 #define PRODUCT_ID_BMP           0x6018
 
+#define VENDOR_ID_STLINK		 0x0483
+#define PRODUCT_ID_STLINK_MASK	 0xffe0
+#define PRODUCT_ID_STLINK_GROUP  0x3740
+#define PRODUCT_ID_STLINKV1		 0x3744
+#define PRODUCT_ID_STLINKV2		 0x3748
+#define PRODUCT_ID_STLINKV21	 0x374b
+#define PRODUCT_ID_STLINKV21_MSD 0x3752
+#define PRODUCT_ID_STLINKV3_BL	 0x374d
+#define PRODUCT_ID_STLINKV3		 0x374f
+#define PRODUCT_ID_STLINKV3E	 0x374e
+
+#define VENDOR_ID_SEGGER         0x1366
+
 typedef enum bmp_type_s {
 	BMP_TYPE_NONE = 0,
 	BMP_TYPE_BMP,
