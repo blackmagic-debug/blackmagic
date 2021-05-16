4e89e27018279a7220ee7b7b23c4cb560b09f48a
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2021-05-16 16:47:34 +0200

    cmsis_dap.h: Fix wrong leftover argument.

--- src/platforms/hosted/cmsis_dap.h
+++ src/platforms/hosted/cmsis_dap.h
@@ -44,7 +44,7 @@ uint32_t dap_swj_clock(uint32_t clock) {return 0;}
 void dap_exit_function(void) {};
 void dap_adiv5_dp_defaults(ADIv5_DP_t *dp) {};
 int cmsis_dap_jtagtap_init(jtag_proc_t *jtag_proc) {return -1;}
-int dap_swdptap_init(swd_proc_t *swd_proc) {return -1;}
+int dap_swdptap_init(ADIv5_DP_t *dp) {return -1;}
 int dap_jtag_dp_init(ADIv5_DP_t *dp) {return -1;}
 void dap_swd_configure(uint8_t cfg) {};
 # pragma GCC diagnostic pop
