5eb43a1ddbb8e5b6aeb51bc8b82bcd4649384d98
aam335 <aam335@gmail.com>
2021-08-02 22:42:00 +0300

    platforms: stm32f4x1 blackpillv2 as variant of f4discovery
    Compile as make PROBE_HOST=f4discovery BLACKPILL=1

--- src/platforms/f4discovery/platform.c
+++ src/platforms/f4discovery/platform.c
@@ -37,20 +37,32 @@
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/cm3/cortex.h>
 
+#ifdef BLACKPILL
+#include <libopencm3/usb/dwc/otg_fs.h>
+#endif
+
+
 jmp_buf fatal_error_jmpbuf;
 extern char _ebss[];
 
 void platform_init(void)
 {
 	volatile uint32_t *magic = (uint32_t *)_ebss;
-	/* Check the USER button*/
+	/* Enable GPIO peripherals */
 	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_GPIOC);
+#ifdef BLACKPILL
+	rcc_periph_clock_enable(RCC_GPIOB);
+#else
+	rcc_periph_clock_enable(RCC_GPIOD);
+#endif
+	/* Check the USER button*/
 	if (gpio_get(GPIOA, GPIO0) ||
-	   ((magic[0] == BOOTMAGIC0) && (magic[1] == BOOTMAGIC1))) {
+		((magic[0] == BOOTMAGIC0) && (magic[1] == BOOTMAGIC1)))
+	{
 		magic[0] = 0;
 		magic[1] = 0;
 		/* Assert blue LED as indicator we are in the bootloader */
-		rcc_periph_clock_enable(RCC_GPIOD);
 		gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT,
 						GPIO_PUPD_NONE, LED_BOOTLOADER);
 		gpio_set(LED_PORT, LED_BOOTLOADER);
@@ -58,44 +70,65 @@ void platform_init(void)
 		   As we just come out of reset, no other deinit is needed!*/
 		rcc_periph_clock_enable(RCC_SYSCFG);
 		SYSCFG_MEMRM &= ~3;
-		SYSCFG_MEMRM |=  1;
+		SYSCFG_MEMRM |= 1;
 		scb_reset_core();
 	}
-
+#ifdef BLACKPILL
+	rcc_clock_setup_pll(&rcc_hse_25mhz_3v3[RCC_CLOCK_3V3_84MHZ]);
+#else
 	rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+#endif
 
 	/* Enable peripherals */
 	rcc_periph_clock_enable(RCC_OTGFS);
-	rcc_periph_clock_enable(RCC_GPIOC);
-	rcc_periph_clock_enable(RCC_GPIOD);
 	rcc_periph_clock_enable(RCC_CRC);
 
 	/* Set up USB Pins and alternate function*/
-	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE,  GPIO9 | GPIO11 | GPIO12);
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9 | GPIO11 | GPIO12);
 	gpio_set_af(GPIOA, GPIO_AF10, GPIO9 | GPIO10 | GPIO11 | GPIO12);
 
-	GPIOC_OSPEEDR &=~0xF30;
+#ifdef BLACKPILL
+	GPIOA_OSPEEDR &= 0x3C00000C;
+	GPIOA_OSPEEDR |= 0x28000008;
+#else
+	GPIOC_OSPEEDR &= ~0xF30;
 	GPIOC_OSPEEDR |= 0xA20;
+#endif
+
 	gpio_mode_setup(JTAG_PORT, GPIO_MODE_OUTPUT,
-			GPIO_PUPD_NONE,
-			TCK_PIN | TDI_PIN);
+					GPIO_PUPD_NONE,
+					TCK_PIN | TDI_PIN);
 	gpio_mode_setup(JTAG_PORT, GPIO_MODE_INPUT,
-			GPIO_PUPD_NONE, TMS_PIN);
+					GPIO_PUPD_NONE, TMS_PIN);
 	gpio_set_output_options(JTAG_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_2MHZ,
 							TCK_PIN | TDI_PIN | TMS_PIN);
 	gpio_mode_setup(TDO_PORT, GPIO_MODE_INPUT,
-			GPIO_PUPD_NONE,
-			TDO_PIN);
+					GPIO_PUPD_NONE,
+					TDO_PIN);
 	gpio_set_output_options(TDO_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_2MHZ,
-							 TDO_PIN| TMS_PIN);
+							TDO_PIN | TMS_PIN);
 
 	gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT,
-			GPIO_PUPD_NONE,
-			LED_UART | LED_IDLE_RUN | LED_ERROR | LED_BOOTLOADER);
+					GPIO_PUPD_NONE,
+					LED_IDLE_RUN | LED_ERROR | LED_BOOTLOADER);
+
+	gpio_mode_setup(LED_PORT_UART, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_UART);
+
+#ifdef PLATFORM_HAS_POWER_SWITCH
+	gpio_set(PWR_BR_PORT, PWR_BR_PIN);
+	gpio_mode_setup(PWR_BR_PORT, GPIO_MODE_OUTPUT,
+					GPIO_PUPD_NONE,
+					PWR_BR_PIN);
+#endif
 
 	platform_timing_init();
 	usbuart_init();
 	cdcacm_init();
+#ifdef BLACKPILL
+	// https://github.com/libopencm3/libopencm3/pull/1256#issuecomment-779424001
+	OTG_FS_GCCFG |= OTG_GCCFG_NOVBUSSENS | OTG_GCCFG_PWRDWN;
+	OTG_FS_GCCFG &= ~(OTG_GCCFG_VBUSBSEN | OTG_GCCFG_VBUSASEN);
+#endif
 }
 
 void platform_srst_set_val(bool assert) { (void)assert; }
@@ -108,8 +141,20 @@ const char *platform_target_voltage(void)
 
 void platform_request_boot(void)
 {
-	uint32_t *magic = (uint32_t *) &_ebss;
+	uint32_t *magic = (uint32_t *)&_ebss;
 	magic[0] = BOOTMAGIC0;
 	magic[1] = BOOTMAGIC1;
 	scb_reset_system();
 }
+
+#ifdef PLATFORM_HAS_POWER_SWITCH
+bool platform_target_get_power(void)
+{
+	return !gpio_get(PWR_BR_PORT, PWR_BR_PIN);
+}
+
+void platform_target_set_power(bool power)
+{
+	gpio_set_val(PWR_BR_PORT, PWR_BR_PIN, !power);
+}
+#endif
