c8e74d3a2a9ebc843469db6b321d9380c6635498
Nicolas Schodet <nico@ni.fr.eu.org>
2022-01-04 09:14:27 +0100

    scripts: remove import from future which imports from the past
    There is no point to support Python 2.

--- scripts/stm32_mem.py
+++ scripts/stm32_mem.py
@@ -18,7 +18,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import print_function
 from time import sleep
 import struct
 import os
