e2b9f995d10de6352527cfdf0ee55c5d54dc4707
dragonmux <git@dragonmux.network>
2022-01-21 15:13:42 -0500

    avr: Implemented handling for DELAY packets as we run so fast the PDI controller generates them

--- src/target/avr_jtagdp.c
+++ src/target/avr_jtagdp.c
@@ -4,6 +4,8 @@
 #include "jtag_scan.h"
 #include "jtagtap.h"
 
+#define PDI_DELAY	0xDBU
+
 void avr_jtag_dp_handler(uint8_t jd_index, uint32_t j_idcode)
 {
 	AVR_DP_t *dp = calloc(1, sizeof(*dp));
@@ -26,19 +28,24 @@ bool avr_jtag_shift_dr(jtag_proc_t *jp, uint8_t jd_index, uint8_t *dout, const u
 	jtag_dev_t *d = &jtag_devs[jd_index];
 	uint8_t result = 0;
 	uint16_t request = 0, response = 0;
-	uint8_t *data = (uint8_t *)&request;
+	uint8_t *data;
 	if (!dout)
 		return false;
-	jtagtap_shift_dr();
-	jp->jtagtap_tdi_seq(0, ones, d->dr_prescan);
-	data[0] = din;
-	// Calculate the parity bit
-	for (uint8_t i = 0; i < 8; ++i)
-		data[1] ^= (din >> i) & 1U;
-	jp->jtagtap_tdi_tdo_seq((uint8_t *)&response, 1, (uint8_t *)&request, 9);
-	jp->jtagtap_tdi_seq(1, ones, d->dr_postscan);
-	jtagtap_return_idle();
-	data = (uint8_t *)&response;
+	do
+	{
+		data = (uint8_t *)&request;
+		jtagtap_shift_dr();
+		jp->jtagtap_tdi_seq(0, ones, d->dr_prescan);
+		data[0] = din;
+		// Calculate the parity bit
+		for (uint8_t i = 0; i < 8; ++i)
+			data[1] ^= (din >> i) & 1U;
+		jp->jtagtap_tdi_tdo_seq((uint8_t *)&response, 1, (uint8_t *)&request, 9);
+		jp->jtagtap_tdi_seq(1, ones, d->dr_postscan);
+		jtagtap_return_idle();
+		data = (uint8_t *)&response;
+	}
+	while (data[0] == PDI_DELAY && data[1] == 1);
 	// Calculate the parity bit
 	for (uint8_t i = 0; i < 8; ++i)
 		result ^= (data[0] >> i) & 1U;
