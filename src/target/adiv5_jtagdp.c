57e6c2fac39c747728c566c669d0d19618c121e0
dragonmux <git@dragonmux.network>
2022-01-21 21:08:21 -0500

    jtag_scan: Properly fixed the wrong IDCode getting to the handlers

--- src/target/adiv5_jtagdp.c
+++ src/target/adiv5_jtagdp.c
@@ -46,9 +46,10 @@ void adiv5_jtag_dp_handler(uint8_t jd_index, uint32_t j_idcode)
 		DEBUG_WARN("calloc: failed in %s\n", __func__);
 		return;
 	}
+	(void)j_idcode;
 
 	dp->dp_jd_index = jd_index;
-	dp->idcode = j_idcode;
+	dp->idcode = jtag_devs[jd_index].jd_idcode;
 	if ((PC_HOSTED == 0 ) || (!platform_jtag_dp_init(dp))) {
 		dp->dp_read = fw_adiv5_jtagdp_read;
 		dp->error = adiv5_jtagdp_error;
