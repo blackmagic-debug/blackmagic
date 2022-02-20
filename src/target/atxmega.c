3779f89da7eeeda2240a91194ee20ba2c56f23b4
dragonmux <git@dragonmux.network>
2022-02-20 00:30:14 -0500

    atxmega: Added configuration to set the max breakpoints on each target

--- src/target/atxmega.c
+++ src/target/atxmega.c
@@ -54,6 +54,8 @@ static const char tdesc_xmega6[] =
 
 bool atxmega_probe(target *t)
 {
+	avr_pdi_t *pdi = t->priv;
+
 	switch (t->idcode) {
 		case IDCODE_XMEGA256A3U:
 			t->core = "ATXMega256A3U";
@@ -65,6 +67,7 @@ bool atxmega_probe(target *t)
 			avr_add_flash(t, 0x00000000, 0x40000);
 			avr_add_flash(t, 0x00040000, 0x2000);
 			t->tdesc = tdesc_xmega6;
+			pdi->hw_breakpoint_max = 2;
 			return true;
 	}
 	return false;
