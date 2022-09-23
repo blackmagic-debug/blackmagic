276da129c6255672146a5b216928cf68baab9ff3
dragonmux <git@dragonmux.network>
2022-09-23 07:54:02 +0100

    morse: run clang-format and fixed the consistency of the code constants

--- src/morse.c
+++ src/morse.c
@@ -25,46 +25,47 @@ static const struct {
 	uint16_t code;
 	uint8_t bits;
 } morse_letter[] = {
-	{        0b00011101,  8}, // 'A' .-
-	{    0b000101010111, 12}, // 'B' -...
-	{  0b00010111010111, 14}, // 'C' -.-.
-	{      0b0001010111, 10}, // 'D' -..
-	{            0b0001,  4}, // 'E' .
-	{    0b000101110101, 12}, // 'F' ..-.
-	{    0b000101110111, 12}, // 'G' --.
-	{      0b0001010101, 10}, // 'H' ....
-	{          0b000101,  6}, // 'I' ..
+	{0b0000000000011101, 8},  // 'A' .-
+	{0b0000000101010111, 12}, // 'B' -...
+	{0b0000010111010111, 14}, // 'C' -.-.
+	{0b0000000001010111, 10}, // 'D' -..
+	{0b0000000000000001, 4},  // 'E' .
+	{0b0000000101110101, 12}, // 'F' ..-.
+	{0b0000000101110111, 12}, // 'G' --.
+	{0b0000000001010101, 10}, // 'H' ....
+	{0b0000000000000101, 6},  // 'I' ..
 	{0b0001110111011101, 16}, // 'J' .---
-	{    0b000111010111, 12}, // 'K' -.-
-	{    0b000101011101, 12}, // 'L' .-..
-	{      0b0001110111, 10}, // 'M' --
-	{        0b00010111,  8}, // 'N' -.
-	{  0b00011101110111, 14}, // 'O' ---
-	{  0b00010111011101, 14}, // 'P' .--.
+	{0b0000000111010111, 12}, // 'K' -.-
+	{0b0000000101011101, 12}, // 'L' .-..
+	{0b0000000001110111, 10}, // 'M' --
+	{0b0000000000010111, 8},  // 'N' -.
+	{0b0000011101110111, 14}, // 'O' ---
+	{0b0000010111011101, 14}, // 'P' .--.
 	{0b0001110101110111, 16}, // 'Q' --.-
-	{      0b0001011101, 10}, // 'R' .-.
-	{        0b00010101,  8}, // 'S' ...
-	{          0b000111,  6}, // 'T' -
-	{      0b0001110101, 10}, // 'U' ..-
-	{    0b000111010101, 12}, // 'V' ...-
-	{    0b000111011101, 12}, // 'W' .--
-	{  0b00011101010111, 14}, // 'X' -..-
+	{0b0000000001011101, 10}, // 'R' .-.
+	{0b0000000000010101, 8},  // 'S' ...
+	{0b0000000000000111, 6},  // 'T' -
+	{0b0000000001110101, 10}, // 'U' ..-
+	{0b0000000111010101, 12}, // 'V' ...-
+	{0b0000000111011101, 12}, // 'W' .--
+	{0b0000011101010111, 14}, // 'X' -..-
 	{0b0001110111010111, 16}, // 'Y' -.--
-	{  0b00010101110111, 14}, // 'Z' --..
+	{0b0000010101110111, 14}, // 'Z' --..
 };
 
-const char *morse_msg;
-static const char * volatile morse_ptr;
-static char morse_repeat;
+const char *morse_msg = NULL;
+static volatile size_t msg_index = SIZE_MAX;
+static bool morse_repeat = false;
 
-void morse(const char *msg, char repeat)
+void morse(const char *const msg, const bool repeat)
 {
 #if PC_HOSTED == 1
 	if (msg)
 		DEBUG_WARN("%s\n", msg);
-	(void) repeat;
+	(void)repeat;
 #else
-	morse_msg = morse_ptr = msg;
+	morse_msg = msg;
+	msg_index = 0;
 	morse_repeat = repeat;
 #endif
 }
@@ -74,33 +75,33 @@ bool morse_update(void)
 	static uint16_t code;
 	static uint8_t bits;
 
-	if (!morse_ptr)
+	if (msg_index == SIZE_MAX)
 		return false;
 
 	if (!bits) {
-		char c = *morse_ptr++;
+		char c = morse_msg[msg_index++];
 		if (!c) {
-			if(morse_repeat) {
-				morse_ptr = morse_msg;
-				c = *morse_ptr++;
+			if (morse_repeat) {
+				c = morse_msg[0];
+				msg_index = 1U;
 			} else {
-				morse_ptr = 0;
+				msg_index = SIZE_MAX;
 				return false;
 			}
 		}
-		if ((c >= 'A') && (c <= 'Z')) {
+		if (c >= 'A' && c <= 'Z') {
 			c -= 'A';
 			code = morse_letter[c].code;
 			bits = morse_letter[c].bits;
 		} else {
-			code = 0; bits = 4;
+			code = 0U;
+			bits = 4U;
 		}
 	}
 
-	bool ret = code & 1;
-	code >>= 1;
-	bits--;
+	const bool result = code & 1U;
+	code >>= 1U;
+	--bits;
 
-	return ret;
+	return result;
 }
-
