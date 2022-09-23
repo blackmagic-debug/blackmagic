0b09808bf6d570a3e1e4588d0f1c2a77d2b98e12
dragonmux <git@dragonmux.network>
2022-09-23 07:59:28 +0100

    crc32: run clang-format

--- src/crc32.c
+++ src/crc32.c
@@ -92,6 +92,7 @@ static const uint32_t crc32_table[] = {
 	0xAFB010B1, 0xAB710D06, 0xA6322BDF, 0xA2F33668,
 	0xBCB4666D, 0xB8757BDA, 0xB5365D03, 0xB1F740B4,
 };
+/* clang-format on */
 
 /* clang-format on */
 
