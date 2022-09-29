4dd1245eacd4dd64f77f5c0ff1bc9e6b0dad6950
dragonmux <git@dragonmux.network>
2022-09-29 11:44:20 +0100

    hosted/serial_unix: run clang-format

--- src/platforms/hosted/serial_unix.c
+++ src/platforms/hosted/serial_unix.c
@@ -30,7 +30,7 @@
 #include "cli.h"
 #include "cortexm.h"
 
-static int fd;  /* File descriptor for connection to GDB remote */
+static int fd; /* File descriptor for connection to GDB remote */
 
 /* A nice routine grabbed from
  * https://stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c
@@ -38,67 +38,67 @@ static int fd;  /* File descriptor for connection to GDB remote */
 static int set_interface_attribs(void)
 {
 	struct termios tty;
-	memset (&tty, 0, sizeof tty);
-	if (tcgetattr (fd, &tty) != 0) {
-      DEBUG_WARN("error %d from tcgetattr", errno);
-      return -1;
-    }
+	memset(&tty, 0, sizeof tty);
+	if (tcgetattr(fd, &tty) != 0) {
+		DEBUG_WARN("error %d from tcgetattr", errno);
+		return -1;
+	}
 
-	tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+	tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; // 8-bit chars
 	// disable IGNBRK for mismatched speed tests; otherwise receive break
 	// as \000 chars
-	tty.c_iflag &= ~IGNBRK;         // disable break processing
-	tty.c_lflag = 0;                // no signaling chars, no echo,
+	tty.c_iflag &= ~IGNBRK; // disable break processing
+	tty.c_lflag = 0;        // no signaling chars, no echo,
 	// no canonical processing
-	tty.c_oflag = 0;                // no remapping, no delays
-	tty.c_cc[VMIN]  = 0;            // read doesn't block
-	tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+	tty.c_oflag = 0;     // no remapping, no delays
+	tty.c_cc[VMIN] = 0;  // read doesn't block
+	tty.c_cc[VTIME] = 5; // 0.5 seconds read timeout
 
 	tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
 
-	tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+	tty.c_cflag |= (CLOCAL | CREAD); // ignore modem controls,
 	// enable reading
 	tty.c_cflag &= ~CSTOPB;
 #if defined(CRTSCTS)
 	tty.c_cflag &= ~CRTSCTS;
 #endif
-	if (tcsetattr (fd, TCSANOW, &tty) != 0) {
+	if (tcsetattr(fd, TCSANOW, &tty) != 0) {
 		DEBUG_WARN("error %d from tcsetattr", errno);
 		return -1;
-    }
+	}
 	return 0;
 }
 
 #ifdef __APPLE__
 int serial_open(BMP_CL_OPTIONS_t *cl_opts, char *serial)
 {
-    char name[4096];
-    if (!cl_opts->opt_device) {
-        /* Try to find some BMP if0*/
-        if (!serial) {
-            DEBUG_WARN("No serial device found\n");
-            return -1;
-        } else {
-            sprintf(name, "/dev/cu.usbmodem%s1", serial);
-        }
-    } else {
-        strncpy(name, cl_opts->opt_device, sizeof(name) - 1);
-    }
-    fd = open(name, O_RDWR | O_SYNC | O_NOCTTY);
-    if (fd < 0) {
-        DEBUG_WARN("Couldn't open serial port %s\n", name);
-        return -1;
-    }
-    /* BMP only offers an USB-Serial connection with no real serial
+	char name[4096];
+	if (!cl_opts->opt_device) {
+		/* Try to find some BMP if0*/
+		if (!serial) {
+			DEBUG_WARN("No serial device found\n");
+			return -1;
+		} else {
+			sprintf(name, "/dev/cu.usbmodem%s1", serial);
+		}
+	} else {
+		strncpy(name, cl_opts->opt_device, sizeof(name) - 1);
+	}
+	fd = open(name, O_RDWR | O_SYNC | O_NOCTTY);
+	if (fd < 0) {
+		DEBUG_WARN("Couldn't open serial port %s\n", name);
+		return -1;
+	}
+	/* BMP only offers an USB-Serial connection with no real serial
      * line in between. No need for baudrate or parity.!
      */
-    return set_interface_attribs();
+	return set_interface_attribs();
 }
 #else
 #define BMP_IDSTRING_BLACKSPHERE "usb-Black_Sphere_Technologies_Black_Magic_Probe"
-#define BMP_IDSTRING_BLACKMAGIC "usb-Black_Magic_Debug_Black_Magic_Probe"
+#define BMP_IDSTRING_BLACKMAGIC  "usb-Black_Magic_Debug_Black_Magic_Probe"
 #define BMP_IDSTRING_1BITSQUARED "usb-1BitSquared_Black_Magic_Probe"
-#define DEVICE_BY_ID "/dev/serial/by-id/"
+#define DEVICE_BY_ID             "/dev/serial/by-id/"
 int serial_open(BMP_CL_OPTIONS_t *cl_opts, char *serial)
 {
 	char name[4096];
@@ -113,9 +113,8 @@ int serial_open(BMP_CL_OPTIONS_t *cl_opts, char *serial)
 		int num_devices = 0;
 		int num_total = 0;
 		while ((dp = readdir(dir)) != NULL) {
-			if ((strstr(dp->d_name, BMP_IDSTRING_BLACKSPHERE) ||
-			     strstr(dp->d_name, BMP_IDSTRING_BLACKMAGIC) ||
-			     strstr(dp->d_name, BMP_IDSTRING_1BITSQUARED)) &&
+			if ((strstr(dp->d_name, BMP_IDSTRING_BLACKSPHERE) || strstr(dp->d_name, BMP_IDSTRING_BLACKMAGIC) ||
+					strstr(dp->d_name, BMP_IDSTRING_1BITSQUARED)) &&
 				(strstr(dp->d_name, "-if00"))) {
 				num_total++;
 				if ((serial) && (!strstr(dp->d_name, serial)))
@@ -126,7 +125,7 @@ int serial_open(BMP_CL_OPTIONS_t *cl_opts, char *serial)
 			}
 		}
 		closedir(dir);
-		if ((num_devices == 0) && (num_total == 0)){
+		if ((num_devices == 0) && (num_total == 0)) {
 			DEBUG_WARN("No BMP probe found\n");
 			return -1;
 		} else if (num_devices != 1) {
@@ -134,9 +133,8 @@ int serial_open(BMP_CL_OPTIONS_t *cl_opts, char *serial)
 			dir = opendir(DEVICE_BY_ID);
 			if (dir) {
 				while ((dp = readdir(dir)) != NULL) {
-					if ((strstr(dp->d_name, BMP_IDSTRING_BLACKSPHERE) ||
-					     strstr(dp->d_name, BMP_IDSTRING_BLACKMAGIC) ||
-					     strstr(dp->d_name, BMP_IDSTRING_1BITSQUARED)) &&
+					if ((strstr(dp->d_name, BMP_IDSTRING_BLACKSPHERE) || strstr(dp->d_name, BMP_IDSTRING_BLACKMAGIC) ||
+							strstr(dp->d_name, BMP_IDSTRING_1BITSQUARED)) &&
 						(strstr(dp->d_name, "-if00")))
 						DEBUG_WARN("%s\n", dp->d_name);
 				}
@@ -158,7 +156,7 @@ int serial_open(BMP_CL_OPTIONS_t *cl_opts, char *serial)
 	if (fd < 0) {
 		DEBUG_WARN("Couldn't open serial port %s\n", name);
 		return -1;
-    }
+	}
 	/* BMP only offers an USB-Serial connection with no real serial
 	 * line in between. No need for baudrate or parity.!
 	 */
@@ -180,7 +178,7 @@ int platform_buffer_write(const uint8_t *data, int size)
 	if (s < 0) {
 		DEBUG_WARN("Failed to write\n");
 		exit(-2);
-    }
+	}
 
 	return size;
 }
@@ -190,13 +188,13 @@ int platform_buffer_read(uint8_t *data, int maxsize)
 	uint8_t *c;
 	int s;
 	int ret;
-	fd_set  rset;
+	fd_set rset;
 	struct timeval tv;
 
 	c = data;
 	tv.tv_sec = 0;
 
-	tv.tv_sec = cortexm_wait_timeout / 1000 ;
+	tv.tv_sec = cortexm_wait_timeout / 1000;
 	tv.tv_usec = 1000 * (cortexm_wait_timeout % 1000);
 
 	/* Look for start of response */
@@ -207,16 +205,15 @@ int platform_buffer_read(uint8_t *data, int maxsize)
 		ret = select(fd + 1, &rset, NULL, NULL, &tv);
 		if (ret < 0) {
 			DEBUG_WARN("Failed on select\n");
-			return(-3);
+			return (-3);
 		}
-		if(ret == 0) {
+		if (ret == 0) {
 			DEBUG_WARN("Timeout on read RESP\n");
-			return(-4);
+			return (-4);
 		}
 
 		s = read(fd, c, 1);
-    }
-	while ((s > 0) && (*c != REMOTE_RESP));
+	} while ((s > 0) && (*c != REMOTE_RESP));
 	/* Now collect the response */
 	do {
 		FD_ZERO(&rset);
@@ -226,21 +223,21 @@ int platform_buffer_read(uint8_t *data, int maxsize)
 			DEBUG_WARN("Failed on select\n");
 			exit(-4);
 		}
-		if(ret == 0) {
+		if (ret == 0) {
 			DEBUG_WARN("Timeout on read\n");
-			return(-5);
+			return (-5);
 		}
 		s = read(fd, c, 1);
-		if (*c==REMOTE_EOM) {
+		if (*c == REMOTE_EOM) {
 			*c = 0;
-			DEBUG_WIRE("       %s\n",data);
+			DEBUG_WIRE("       %s\n", data);
 			return (c - data);
 		} else {
 			c++;
 		}
-	}while ((s >= 0) && ((c - data) < maxsize));
+	} while ((s >= 0) && ((c - data) < maxsize));
 
 	DEBUG_WARN("Failed to read\n");
-	return(-6);
+	return (-6);
 	return 0;
 }
