4f4d7cfdae24418783aa1426b0f8f953ee74d9e6
dragonmux <git@dragonmux.network>
2022-10-07 01:02:44 +0100

    hosted/libftdi_jtagtap: Refactored libftdi_jtagtap_init()

--- src/platforms/hosted/libftdi_jtagtap.c
+++ src/platforms/hosted/libftdi_jtagtap.c
@@ -18,8 +18,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* Low level JTAG implementation using FT2232 with libftdi.
- *
+/*
+ * Low level JTAG implementation using FT2232 with libftdi.
  */
 
 #include "general.h"
@@ -28,60 +28,64 @@
 #include <ftdi.h>
 #include "ftdi_bmp.h"
 
-extern cable_desc_t *active_cable;
-extern struct ftdi_context *ftdic;
-
 static void jtagtap_reset(void);
 static void jtagtap_tms_seq(uint32_t tms_states, size_t clock_cycles);
 static void jtagtap_tdi_seq(bool final_tms, const uint8_t *data_in, size_t clock_cycles);
 static bool jtagtap_next(bool tms, bool tdi);
 
-int libftdi_jtagtap_init(jtag_proc_t *jtag_proc)
+void libftdi_drain_potential_garbage(void)
 {
-	if ((active_cable->mpsse_swd_read.set_data_low == MPSSE_DO) &&
-		(active_cable->mpsse_swd_write.set_data_low == MPSSE_DO)) {
-		printf("Jtag not possible with resistor SWD!\n");
+	uint8_t data[16];
+	int garbage = ftdi_read_data(ftdic, data, sizeof(data));
+	if (garbage > 0) {
+		DEBUG_WARN("FTDI JTAG init got garbage:");
+		for (int i = 0; i < garbage; i++)
+			DEBUG_WARN(" %02x", data[i]);
+		DEBUG_WARN("\n");
+	}
+}
+
+int libftdi_jtagtap_init(void)
+{
+	if (active_cable->mpsse_swd_read.set_data_low == MPSSE_DO &&
+		active_cable->mpsse_swd_write.set_data_low == MPSSE_DO) {
+		DEBUG_WARN("JTAG not possible with resistor SWD!\n");
 		return -1;
 	}
-	jtag_proc->jtagtap_reset = jtagtap_reset;
-	jtag_proc->jtagtap_next = jtagtap_next;
-	jtag_proc->jtagtap_tms_seq = jtagtap_tms_seq;
-	jtag_proc->jtagtap_tdi_tdo_seq = libftdi_jtagtap_tdi_tdo_seq;
-	jtag_proc->jtagtap_tdi_seq = jtagtap_tdi_seq;
+
+	jtag_proc.jtagtap_reset = jtagtap_reset;
+	jtag_proc.jtagtap_next = jtagtap_next;
+	jtag_proc.jtagtap_tms_seq = jtagtap_tms_seq;
+	jtag_proc.jtagtap_tdi_tdo_seq = libftdi_jtagtap_tdi_tdo_seq;
+	jtag_proc.jtagtap_tdi_seq = jtagtap_tdi_seq;
 
 	active_state.data_low |= active_cable->jtag.set_data_low | MPSSE_CS | MPSSE_DI | MPSSE_DO;
 	active_state.data_low &= ~(active_cable->jtag.clr_data_low | MPSSE_SK);
 	active_state.ddr_low |= MPSSE_CS | MPSSE_DO | MPSSE_SK;
-	active_state.ddr_low &= ~(MPSSE_DI);
+	active_state.ddr_low &= ~MPSSE_DI;
 	active_state.data_high |= active_cable->jtag.set_data_high;
-	active_state.data_high &= ~(active_cable->jtag.clr_data_high);
-	uint8_t gab[16];
-	int garbage = ftdi_read_data(ftdic, gab, sizeof(gab));
-	if (garbage > 0) {
-		DEBUG_WARN("FTDI JTAG init got garbage:");
-		for (int i = 0; i < garbage; i++)
-			DEBUG_WARN(" %02x", gab[i]);
-		DEBUG_WARN("\n");
-	}
-	uint8_t cmd_write[16] = {SET_BITS_LOW, active_state.data_low, active_state.ddr_low, SET_BITS_HIGH,
-		active_state.data_high, active_state.ddr_high};
-	libftdi_buffer_write(cmd_write, 6);
+	active_state.data_high &= ~active_cable->jtag.clr_data_high;
+	libftdi_drain_potential_garbage();
+
+	const uint8_t cmd[6] = {
+		SET_BITS_LOW,
+		active_state.data_low,
+		active_state.ddr_low,
+		SET_BITS_HIGH,
+		active_state.data_high,
+		active_state.ddr_high,
+	};
+	libftdi_buffer_write_arr(cmd);
 	libftdi_buffer_flush();
 	/* Write out start condition and pull garbage from read buffer.
 	 * FT2232D otherwise misbehaves on runs follwoing the first run.*/
-	garbage = ftdi_read_data(ftdic, cmd_write, sizeof(cmd_write));
-	if (garbage > 0) {
-		DEBUG_WARN("FTDI JTAG end init got garbage:");
-		for (int i = 0; i < garbage; i++)
-			DEBUG_WARN(" %02x", cmd_write[i]);
-		DEBUG_WARN("\n");
-	}
-	/* Go to JTAG mode for SWJ-DP */
-	for (int i = 0; i <= 50; i++)
-		jtag_proc->jtagtap_next(1, 0);      /* Reset SW-DP */
-	jtag_proc->jtagtap_tms_seq(0xE73C, 16); /* SWD to JTAG sequence */
-	jtag_proc->jtagtap_tms_seq(0x1F, 6);
+	libftdi_drain_potential_garbage();
 
+	/* Ensure we're in JTAG mode */
+	for (size_t i = 0; i <= 50; ++i)
+		jtagtap_next(true, false); /* 50 idle cylces for SWD reset */
+	jtagtap_tms_seq(0xe73cU, 16);  /* SWD to JTAG sequence */
+	jtagtap_soft_reset();
 	return 0;
 }
 
