535590507243097f94d552c1a0acf06a97d4b77a
dragonmux <git@dragonmux.network>
2022-10-13 21:08:15 +0100

    jtag_scan: Naming cleanup for the jtag_dev_{write_ir,shift_dr} functions

--- src/target/jtag_scan.h
+++ src/target/jtag_scan.h
@@ -24,8 +24,8 @@
 #include <stddef.h>
 #include "jtagtap.h"
 
-#define JTAG_MAX_DEVS   32
-#define JTAG_MAX_IR_LEN 16
+#define JTAG_MAX_DEVS   32U
+#define JTAG_MAX_IR_LEN 16U
 
 typedef struct jtag_dev {
 	const char *jd_descr;
@@ -43,7 +43,7 @@ typedef struct jtag_dev {
 	uint8_t ir_postscan;
 } jtag_dev_s;
 
-extern jtag_dev_s jtag_devs[JTAG_MAX_DEVS + 1];
+extern jtag_dev_s jtag_devs[JTAG_MAX_DEVS + 1U];
 extern uint32_t jtag_dev_count;
 
 void jtag_dev_write_ir(jtag_proc_t *jp, uint8_t jd_index, uint32_t ir);
