a450dafa740454e3e7a5ddee7c7e2a5bdd5b3808
dragonmux <git@dragonmux.network>
2022-10-13 21:18:58 +0100

    jtagtap: Corrected the naming of struct jtag_proc and typedef jtag_proc_s

--- src/platforms/hosted/cmsis_dap.h
+++ src/platforms/hosted/cmsis_dap.h
@@ -27,7 +27,7 @@
 int dap_init(bmp_info_t *info);
 void dap_exit_function(void);
 void dap_adiv5_dp_defaults(ADIv5_DP_t *dp);
-int cmsis_dap_jtagtap_init(jtag_proc_t *jtag_proc);
+int cmsis_dap_jtagtap_init(jtag_proc_s *jtag_proc);
 int dap_swdptap_init(ADIv5_DP_t *dp);
 int dap_jtag_dp_init(ADIv5_DP_t *dp);
 uint32_t dap_swj_clock(uint32_t clock);
@@ -45,7 +45,7 @@ int dap_init(bmp_info_t *info)
 uint32_t dap_swj_clock(uint32_t clock) { return 0; }
 void dap_exit_function(void) { }
 void dap_adiv5_dp_defaults(ADIv5_DP_t *dp) { }
-int cmsis_dap_jtagtap_init(jtag_proc_t *jtag_proc) { return -1; }
+int cmsis_dap_jtagtap_init(jtag_proc_s *jtag_proc) { return -1; }
 int dap_swdptap_init(ADIv5_DP_t *dp) { return -1; }
 int dap_jtag_dp_init(ADIv5_DP_t *dp) { return -1; }
 void dap_swd_configure(uint8_t cfg) { }
