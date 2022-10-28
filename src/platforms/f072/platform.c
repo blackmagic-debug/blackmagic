b8ca0cb7186786c9589db2889adc4a2b77dbc55d
dragonmux <git@dragonmux.network>
2022-10-28 16:53:54 +0100

    f072: Run clang-format across the platform

--- src/platforms/f072/platform.c
+++ src/platforms/f072/platform.c
@@ -36,12 +36,12 @@
 
 extern uint32_t _ebss[];
 
-#define SYSCFG_MEMRM                  MMIO32(0x40010000)
-#define SYSMEM_RESET_VECTOR            0x1fffC804
+#define SYSCFG_MEMRM        MMIO32(0x40010000)
+#define SYSMEM_RESET_VECTOR 0x1fffC804
 
 void platform_init(void)
 {
-	volatile uint32_t *magic = (uint32_t *) &_ebss;
+	volatile uint32_t *magic = (uint32_t *)&_ebss;
 	/* If RCC_CFGR is not at it's reset value, the bootloader was executed
 	 * and SET_ADDRESS got us to this place. On F3 ???, without further efforts,
 	 * DFU does not start in that case.
@@ -56,12 +56,12 @@ void platform_init(void)
 		magic[1] = 0;
 		/* Jump to the built in bootloader by mapping System flash.
 		   As we just come out of reset, no other deinit is needed!*/
-		SYSCFG_MEMRM |=  1;
-		void (*bootloader)(void) =
-			(void (*)(void)) (*((uint32_t *) SYSMEM_RESET_VECTOR));
+		SYSCFG_MEMRM |= 1;
+		void (*bootloader)(void) = (void (*)(void))(*((uint32_t *)SYSMEM_RESET_VECTOR));
 		/* We come out of reset, so MSP is already set*/
 		bootloader();
-		while (1);
+		while (1)
+			;
 	}
 	rcc_clock_setup_in_hse_8mhz_out_48mhz();
 
@@ -73,16 +73,12 @@ void platform_init(void)
 
 	GPIOA_OSPEEDR &= ~0xF00C;
 	GPIOA_OSPEEDR |= 0x5004; /* Set medium speed on PA1, PA6,PA7*/
-	gpio_mode_setup(JTAG_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
-					TMS_PIN | TCK_PIN |TDI_PIN);
+	gpio_mode_setup(JTAG_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, TMS_PIN | TCK_PIN | TDI_PIN);
 	gpio_mode_setup(TDO_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, TDO_PIN);
-	gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT,
-					GPIO_PUPD_NONE,
-					LED_UART | LED_IDLE_RUN | LED_ERROR | LED_BOOTLOADER);
+	gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_UART | LED_IDLE_RUN | LED_ERROR | LED_BOOTLOADER);
 	gpio_mode_setup(NRST_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, NRST_PIN);
 	gpio_set(NRST_PORT, NRST_PIN);
-	gpio_set_output_options(NRST_PORT, GPIO_OTYPE_OD,
-							GPIO_OSPEED_2MHZ, NRST_PIN);
+	gpio_set_output_options(NRST_PORT, GPIO_OTYPE_OD, GPIO_OSPEED_2MHZ, NRST_PIN);
 	platform_timing_init();
 	blackmagic_usb_init();
 	aux_serial_init();
@@ -105,7 +101,7 @@ const char *platform_target_voltage(void)
 
 void platform_request_boot(void)
 {
-	uint32_t *magic = (uint32_t *) &_ebss;
+	uint32_t *magic = (uint32_t *)&_ebss;
 	magic[0] = BOOTMAGIC0;
 	magic[1] = BOOTMAGIC1;
 	scb_reset_system();
--- src/platforms/f072/platform.h
+++ src/platforms/f072/platform.h
@@ -31,7 +31,7 @@
 
 #define PLATFORM_HAS_TRACESWO
 
-#define PLATFORM_IDENT   "(F072-IF) "
+#define PLATFORM_IDENT "(F072-IF) "
 
 /* Important pin mappings for STM32 implementation:
  *
@@ -49,45 +49,42 @@
  */
 
 /* Hardware definitions... */
-#define JTAG_PORT 	GPIOA
-#define TDI_PORT	JTAG_PORT
-#define TMS_PORT	JTAG_PORT
-#define TCK_PORT	JTAG_PORT
-#define TDO_PORT	JTAG_PORT
-#define TDI_PIN		GPIO0
-#define TMS_PIN		GPIO1
-#define TCK_PIN		GPIO7
-#define TDO_PIN		GPIO6
+#define JTAG_PORT GPIOA
+#define TDI_PORT  JTAG_PORT
+#define TMS_PORT  JTAG_PORT
+#define TCK_PORT  JTAG_PORT
+#define TDO_PORT  JTAG_PORT
+#define TDI_PIN   GPIO0
+#define TMS_PIN   GPIO1
+#define TCK_PIN   GPIO7
+#define TDO_PIN   GPIO6
 
-#define SWDIO_PORT 	JTAG_PORT
-#define SWCLK_PORT 	JTAG_PORT
-#define SWDIO_PIN	TMS_PIN
-#define SWCLK_PIN	TCK_PIN
+#define SWDIO_PORT JTAG_PORT
+#define SWCLK_PORT JTAG_PORT
+#define SWDIO_PIN  TMS_PIN
+#define SWCLK_PIN  TCK_PIN
 
-#define NRST_PORT	GPIOA
-#define NRST_PIN	GPIO5
+#define NRST_PORT GPIOA
+#define NRST_PIN  GPIO5
 
-#define LED_PORT	GPIOB
-#define LED_PORT_UART	GPIOB
-#define LED_UART	GPIO6
-#define LED_IDLE_RUN	GPIO5
-#define LED_ERROR	GPIO7
+#define LED_PORT      GPIOB
+#define LED_PORT_UART GPIOB
+#define LED_UART      GPIO6
+#define LED_IDLE_RUN  GPIO5
+#define LED_ERROR     GPIO7
 /* PORTB does not stay active in system bootloader!*/
-#define LED_BOOTLOADER	GPIO6
+#define LED_BOOTLOADER GPIO6
 
 #define BOOTMAGIC0 0xb007da7a
 #define BOOTMAGIC1 0xbaadfeed
 
-#define TMS_SET_MODE()							\
-	gpio_mode_setup(TMS_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, TMS_PIN);
-#define SWDIO_MODE_FLOAT() \
-	gpio_mode_setup(SWDIO_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, SWDIO_PIN);
-#define SWDIO_MODE_DRIVE() \
-	gpio_mode_setup(SWDIO_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, SWDIO_PIN);
+#define TMS_SET_MODE()     gpio_mode_setup(TMS_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, TMS_PIN);
+#define SWDIO_MODE_FLOAT() gpio_mode_setup(SWDIO_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, SWDIO_PIN);
+#define SWDIO_MODE_DRIVE() gpio_mode_setup(SWDIO_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, SWDIO_PIN);
 
-#define USB_DRIVER      st_usbfs_v2_usb_driver
-#define USB_IRQ         NVIC_USB_IRQ
-#define USB_ISR(x)      usb_isr(x)
+#define USB_DRIVER st_usbfs_v2_usb_driver
+#define USB_IRQ    NVIC_USB_IRQ
+#define USB_ISR(x) usb_isr(x)
 
 /* Interrupt priorities.  Low numbers are high priority.
  * For now USART1 preempts USB which may spin while buffer is drained.
@@ -98,54 +95,62 @@
 #define IRQ_PRI_USBUSART_DMA (2 << 6)
 #define IRQ_PRI_TRACE        (0 << 6)
 
-#define USBUSART USART2
-#define USBUSART_CR1 USART2_CR1
-#define USBUSART_TDR USART2_TDR
-#define USBUSART_RDR USART2_RDR
-#define USBUSART_IRQ NVIC_USART2_IRQ
-#define USBUSART_CLK RCC_USART2
-#define USBUSART_PORT GPIOA
-#define USBUSART_TX_PIN  GPIO3
-#define USBUSART_RX_PIN  GPIO2
+#define USBUSART        USART2
+#define USBUSART_CR1    USART2_CR1
+#define USBUSART_TDR    USART2_TDR
+#define USBUSART_RDR    USART2_RDR
+#define USBUSART_IRQ    NVIC_USART2_IRQ
+#define USBUSART_CLK    RCC_USART2
+#define USBUSART_PORT   GPIOA
+#define USBUSART_TX_PIN GPIO3
+#define USBUSART_RX_PIN GPIO2
 #define USBUSART_ISR(x) usart2_isr(x)
 
 #define USBUSART_DMA_BUS DMA1
 #define USBUSART_DMA_CLK RCC_DMA1
 /* This needs corresponding remapping bit cleared in the SYSCFG_CFGR1.
  * As we come out of reset, the bit is cleared!*/
-#define USBUSART_DMA_TX_CHAN DMA_CHANNEL4
-#define USBUSART_DMA_TX_ISR(x) dma1_channel4_7_isr(x)
-#define USBUSART_DMA_RX_CHAN DMA_CHANNEL5
-#define USBUSART_DMA_RX_ISR(x) dma2_channel3_5_isr(x)
-#define USBUSART_DMA_RXTX_IRQ NVIC_DMA1_CHANNEL4_7_DMA2_CHANNEL3_5_IRQ
+#define USBUSART_DMA_TX_CHAN     DMA_CHANNEL4
+#define USBUSART_DMA_TX_ISR(x)   dma1_channel4_7_isr(x)
+#define USBUSART_DMA_RX_CHAN     DMA_CHANNEL5
+#define USBUSART_DMA_RX_ISR(x)   dma2_channel3_5_isr(x)
+#define USBUSART_DMA_RXTX_IRQ    NVIC_DMA1_CHANNEL4_7_DMA2_CHANNEL3_5_IRQ
 #define USBUSART_DMA_RXTX_ISR(x) dma1_channel4_7_dma2_channel3_5_isr(x)
 
 #define STK_CSR_CLKSOURCE_AHB_DIV8 STK_CSR_CLKSOURCE_AHB
 
 /* TX/RX on the REV 0/1 boards are swapped against ftdijtag.*/
-#define UART_PIN_SETUP() do {											\
-		gpio_mode_setup(USBUSART_PORT, GPIO_MODE_AF, GPIO_PUPD_PULLUP,	\
-						USBUSART_TX_PIN | USBUSART_RX_PIN);				\
-		gpio_set_af(USBUSART_PORT, GPIO_AF1,							\
-					USBUSART_TX_PIN | USBUSART_RX_PIN);					\
-		USART2_CR2 |= USART_CR2_SWAP;									\
-	} while(0)
+#define UART_PIN_SETUP()                                                                                   \
+	do {                                                                                                   \
+		gpio_mode_setup(USBUSART_PORT, GPIO_MODE_AF, GPIO_PUPD_PULLUP, USBUSART_TX_PIN | USBUSART_RX_PIN); \
+		gpio_set_af(USBUSART_PORT, GPIO_AF1, USBUSART_TX_PIN | USBUSART_RX_PIN);                           \
+		USART2_CR2 |= USART_CR2_SWAP;                                                                      \
+	} while (0)
 
-#define TRACE_TIM TIM3
+#define TRACE_TIM          TIM3
 #define TRACE_TIM_CLK_EN() rcc_periph_clock_enable(RCC_TIM3)
-#define TRACE_IRQ   NVIC_TIM3_IRQ
-#define TRACE_ISR   tim3_isr
+#define TRACE_IRQ          NVIC_TIM3_IRQ
+#define TRACE_ISR          tim3_isr
 
 #ifdef ENABLE_DEBUG
 extern bool debug_bmp;
-# define DEBUG printf
+#define DEBUG printf
 #else
-# define DEBUG(...)
+#define DEBUG(...)
 #endif
 
-#define SET_RUN_STATE(state)	{running_status = (state);}
-#define SET_IDLE_STATE(state)	{gpio_set_val(LED_PORT, LED_IDLE_RUN, state);}
-#define SET_ERROR_STATE(state)	{gpio_set_val(LED_PORT, LED_ERROR, state);}
+#define SET_RUN_STATE(state)      \
+	{                             \
+		running_status = (state); \
+	}
+#define SET_IDLE_STATE(state)                        \
+	{                                                \
+		gpio_set_val(LED_PORT, LED_IDLE_RUN, state); \
+	}
+#define SET_ERROR_STATE(state)                    \
+	{                                             \
+		gpio_set_val(LED_PORT, LED_ERROR, state); \
+	}
 
 static inline int platform_hwversion(void)
 {
@@ -153,9 +158,9 @@ static inline int platform_hwversion(void)
 }
 
 /* Use newlib provided integer only stdio functions */
-#define sscanf siscanf
-#define sprintf siprintf
+#define sscanf    siscanf
+#define sprintf   siprintf
 #define vasprintf vasiprintf
-#define snprintf sniprintf
+#define snprintf  sniprintf
 
 #endif /* PLATFORMS_F072_PLATFORM_H */
