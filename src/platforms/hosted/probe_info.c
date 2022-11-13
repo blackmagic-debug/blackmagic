815c284f3949f17c5490d807440eae5d34236bf0
Sid Price <sid@sidprice.com>
2022-11-13 08:51:05 -0700

    hosted/probe_info: Guard copying of vid and pid to bmp info structure

--- src/platforms/hosted/probe_info.c
+++ src/platforms/hosted/probe_info.c
@@ -113,8 +113,10 @@ const probe_info_s *probe_info_filter(const probe_info_s *const list, const char
 void probe_info_to_bmp_info(const probe_info_s *const probe, bmp_info_s *info)
 {
 	info->bmp_type = probe->type;
+#if HOSTED_BMP_ONLY != 1
 	info->pid = probe->pid;
 	info->vid = probe->vid;
+#endif
 	const size_t serial_len = MIN(strlen(probe->serial), sizeof(info->serial) - 1U);
 	memcpy(info->serial, probe->serial, serial_len);
 	info->serial[serial_len] = '\0';
