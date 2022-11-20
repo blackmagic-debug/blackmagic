831802a96bd00d595b7235df271d8b17ec1fb2b2
Sid Price <sid@sidprice.com>
2022-11-20 16:05:03 -0700

    ftdi_bmp: fix bmda_cli_options name

--- src/platforms/hosted/ftdi_bmp.h
+++ src/platforms/hosted/ftdi_bmp.h
@@ -178,7 +178,7 @@ extern cable_desc_s active_cable;
 extern ftdi_context_s *ftdic;
 extern data_desc_s active_state;
 
-int ftdi_bmp_init(bmp_cli_options_s *cl_opts, bmp_info_s *info);
+int ftdi_bmp_init(bmda_cli_options_s *cl_opts, bmp_info_s *info);
 int libftdi_swdptap_init(adiv5_debug_port_s *dp);
 bool libftdi_jtagtap_init(void);
 void libftdi_buffer_flush(void);
