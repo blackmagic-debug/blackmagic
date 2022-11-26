blackmagic-debug/blackmagic

commit eedf21e82112147f153372cb08d1514d889c1d82

usb_serial: run clang-format on the code

src/platforms/common/usb_serial.c
@@ -86,10 +86,10 @@ static enum usbd_request_return_codes debug_uart_control_request(usbd_device *de
 	case USB_CDC_REQ_SET_CONTROL_LINE_STATE:
 		usb_serial_set_state(dev, req->wIndex, CDCACM_UART_ENDPOINT);
 #ifdef USBUSART_DTR_PIN
-		gpio_set_val(USBUSART_PORT, USBUSART_DTR_PIN, !(req->wValue & 1));
+		gpio_set_val(USBUSART_PORT, USBUSART_DTR_PIN, !(req->wValue & 1U));
 #endif
 #ifdef USBUSART_RTS_PIN
-		gpio_set_val(USBUSART_PORT, USBUSART_RTS_PIN, !((req->wValue >> 1) & 1));
+		gpio_set_val(USBUSART_PORT, USBUSART_RTS_PIN, !((req->wValue >> 1U) & 1U));
 #endif
 		return USBD_REQ_HANDLED;
 	case USB_CDC_REQ_SET_LINE_CODING:
@@ -121,36 +121,28 @@ void usb_serial_set_state(usbd_device *const dev, const uint16_t iface, const ui
 	usbd_ep_write_packet(dev, ep, buf, sizeof(buf));
 }
 
-void usb_serial_set_config(usbd_device *dev, uint16_t wValue)
+void usb_serial_set_config(usbd_device *dev, uint16_t value)
 {
-	usb_config = wValue;
+	usb_config = value;
 
 	/* GDB interface */
 #if defined(STM32F4) || defined(LM4F)
-	usbd_ep_setup(dev, CDCACM_GDB_ENDPOINT, USB_ENDPOINT_ATTR_BULK,
-	              CDCACM_PACKET_SIZE, gdb_usb_out_cb);
+	usbd_ep_setup(dev, CDCACM_GDB_ENDPOINT, USB_ENDPOINT_ATTR_BULK, CDCACM_PACKET_SIZE, gdb_usb_out_cb);
 #else
-	usbd_ep_setup(dev, CDCACM_GDB_ENDPOINT, USB_ENDPOINT_ATTR_BULK,
-	              CDCACM_PACKET_SIZE, NULL);
+	usbd_ep_setup(dev, CDCACM_GDB_ENDPOINT, USB_ENDPOINT_ATTR_BULK, CDCACM_PACKET_SIZE, NULL);
 #endif
-	usbd_ep_setup(dev, CDCACM_GDB_ENDPOINT | USB_REQ_TYPE_IN,
-				  USB_ENDPOINT_ATTR_BULK, CDCACM_PACKET_SIZE, NULL);
-	usbd_ep_setup(dev, (CDCACM_GDB_ENDPOINT + 1) | USB_REQ_TYPE_IN,
-				  USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
+	usbd_ep_setup(dev, CDCACM_GDB_ENDPOINT | USB_REQ_TYPE_IN, USB_ENDPOINT_ATTR_BULK, CDCACM_PACKET_SIZE, NULL);
+	usbd_ep_setup(dev, (CDCACM_GDB_ENDPOINT + 1) | USB_REQ_TYPE_IN, USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
 
 	/* Serial interface */
-	usbd_ep_setup(dev, CDCACM_UART_ENDPOINT, USB_ENDPOINT_ATTR_BULK,
-	              CDCACM_PACKET_SIZE / 2, usbuart_usb_out_cb);
-	usbd_ep_setup(dev, CDCACM_UART_ENDPOINT | USB_REQ_TYPE_IN,
-				  USB_ENDPOINT_ATTR_BULK,
-	              CDCACM_PACKET_SIZE, usbuart_usb_in_cb);
-	usbd_ep_setup(dev, (CDCACM_UART_ENDPOINT + 1) | USB_REQ_TYPE_IN,
-				  USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
+	usbd_ep_setup(dev, CDCACM_UART_ENDPOINT, USB_ENDPOINT_ATTR_BULK, CDCACM_PACKET_SIZE / 2, usbuart_usb_out_cb);
+	usbd_ep_setup(
+		dev, CDCACM_UART_ENDPOINT | USB_REQ_TYPE_IN, USB_ENDPOINT_ATTR_BULK, CDCACM_PACKET_SIZE, usbuart_usb_in_cb);
+	usbd_ep_setup(dev, (CDCACM_UART_ENDPOINT + 1) | USB_REQ_TYPE_IN, USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
 
 #if defined(PLATFORM_HAS_TRACESWO)
 	/* Trace interface */
-	usbd_ep_setup(dev, TRACE_ENDPOINT | USB_REQ_TYPE_IN, USB_ENDPOINT_ATTR_BULK,
-					64, trace_buf_drain);
+	usbd_ep_setup(dev, TRACE_ENDPOINT | USB_REQ_TYPE_IN, USB_ENDPOINT_ATTR_BULK, 64, trace_buf_drain);
 #endif
 
 	usbd_register_control_callback(dev, USB_REQ_TYPE_CLASS | USB_REQ_TYPE_INTERFACE,

src/platforms/common/usb_serial.h
@@ -32,7 +32,7 @@
 #include <stdbool.h>
 #include "usb.h"
 
-void usb_serial_set_config(usbd_device *dev, uint16_t wValue);
+void usb_serial_set_config(usbd_device *dev, uint16_t value);
 
 bool gdb_uart_get_dtr(void);
 

commit 3903043bc6f1ddf71dde3d6759be42ba0d4c543a

common/usb_serial: Fixed a regression caused in #1190 that puts the CDC notification replies out over the wrong endpoint

src/platforms/common/usb_serial.c
@@ -95,7 +95,8 @@ static usbd_request_return_codes_e gdb_serial_control_request(usbd_device *dev, 
 
 	switch (req->bRequest) {
 	case USB_CDC_REQ_SET_CONTROL_LINE_STATE:
-		usb_serial_set_state(dev, req->wIndex, CDCACM_GDB_ENDPOINT);
+		/* Send a notification back on the notification endpoint */
+		usb_serial_set_state(dev, req->wIndex, CDCACM_GDB_ENDPOINT + 1U);
 		gdb_serial_dtr = req->wValue & 1U;
 		return USBD_REQ_HANDLED;
 	case USB_CDC_REQ_SET_LINE_CODING:
@@ -121,7 +122,8 @@ static usbd_request_return_codes_e debug_serial_control_request(usbd_device *dev
 
 	switch (req->bRequest) {
 	case USB_CDC_REQ_SET_CONTROL_LINE_STATE:
-		usb_serial_set_state(dev, req->wIndex, CDCACM_UART_ENDPOINT);
+		/* Send a notification back on the notification endpoint */
+		usb_serial_set_state(dev, req->wIndex, CDCACM_UART_ENDPOINT + 1U);
 #ifdef USBUSART_DTR_PIN
 		gpio_set_val(USBUSART_PORT, USBUSART_DTR_PIN, !(req->wValue & 1U));
 #endif