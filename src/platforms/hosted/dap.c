blackmagic-debug/blackmagic

commit 52cad38fac2c1153293bd0e3542db796485553e6

hosted/dap: Cleaned up some of the link reset pointer maths

src/platforms/hosted/dap.c
@@ -489,7 +489,7 @@ void dap_reset_link(bool jtag)
 		*p++ = 0x3c;
 		*p++ = 0xe7;
 		*p++ = 0x1f;
-		buf[1] = ((p - &buf[2]) * 8) - 2;
+		buf[1] = ((p - buf + 2) * 8) - 2;
 	} else {
 		*p++ = 0x9e;
 		*p++ = 0xe7;
@@ -501,7 +501,7 @@ void dap_reset_link(bool jtag)
 		*p++ = 0xff;
 		*p++ = 0xff;
 		*p++ = 0x00;
-		buf[1] = (p - &buf[2]) * 8;
+		buf[1] = (p - buf + 2) * 8;
 	}
 	dbg_dap_cmd(buf, sizeof(buf), p - buf);
 

commit 70e110559004c79cc3ac01077b3cca10a7872590

hosted/dap: Fixed error in link reset pointer math.
This fixes a regression introduced in 52cad38.

src/platforms/hosted/dap.c
@@ -449,7 +449,7 @@ void dap_reset_link(bool jtag)
 		*p++ = 0x3c;
 		*p++ = 0xe7;
 		*p++ = 0x1f;
-		buf[1] = ((p - buf + 2U) * 8U) - 2U;
+		buf[1] = ((p - (buf + 2U)) * 8U) - 2U;
 	} else {
 		*p++ = 0x9e;
 		*p++ = 0xe7;
@@ -461,7 +461,7 @@ void dap_reset_link(bool jtag)
 		*p++ = 0xff;
 		*p++ = 0xff;
 		*p++ = 0x00;
-		buf[1] = (p - buf + 2U) * 8U;
+		buf[1] = (p - (buf + 2U)) * 8U;
 	}
 	dbg_dap_cmd(buf, sizeof(buf), p - buf);
 