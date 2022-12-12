49b0e795e3a0d17bec9585926faffbd5556c525c
dragonmux <git@dragonmux.network>
2022-12-12 19:55:25 +0000

    adiv5_swdp: Moved the error handler onto the new checkless DP read and write functions, implementing use of the protocol recovery parameter

--- src/target/adiv5_swdp.c
+++ src/target/adiv5_swdp.c
@@ -207,16 +207,17 @@ uint32_t firmware_swdp_read(adiv5_debug_port_s *dp, uint16_t addr)
 
 uint32_t firmware_swdp_error(adiv5_debug_port_s *dp, const bool protocol_recovery)
 {
-	if (dp->version >= 2 && dp->dp_low_write) {
+	if ((dp->version >= 2 && dp->dp_low_write) || protocol_recovery) {
 		/* On protocol error target gets deselected.
 		 * With DP Change, another target needs selection.
 		 * => Reselect with right target! */
 		dp_line_reset(dp);
-		dp->dp_low_write(dp, ADIV5_DP_TARGETSEL, dp->targetsel);
-		dp->dp_read(dp, ADIV5_DP_DPIDR);
+		firmware_dp_low_read(dp, ADIV5_DP_DPIDR);
+		if (dp->dp_low_write)
+			dp->dp_low_write(dp, ADIV5_DP_TARGETSEL, dp->targetsel);
 		/* Exception here is unexpected, so do not catch */
 	}
-	const uint32_t err = adiv5_dp_read(dp, ADIV5_DP_CTRLSTAT) &
+	const uint32_t err = firmware_dp_low_read(dp, ADIV5_DP_CTRLSTAT) &
 		(ADIV5_DP_CTRLSTAT_STICKYORUN | ADIV5_DP_CTRLSTAT_STICKYCMP | ADIV5_DP_CTRLSTAT_STICKYERR |
 			ADIV5_DP_CTRLSTAT_WDATAERR);
 	uint32_t clr = 0;
@@ -230,9 +231,8 @@ uint32_t firmware_swdp_error(adiv5_debug_port_s *dp, const bool protocol_recover
 	if (err & ADIV5_DP_CTRLSTAT_WDATAERR)
 		clr |= ADIV5_DP_ABORT_WDERRCLR;
 
-	adiv5_dp_write(dp, ADIV5_DP_ABORT, clr);
+	firmware_dp_low_write(dp, ADIV5_DP_ABORT, clr);
 	dp->fault = 0;
-
 	return err;
 }
 
