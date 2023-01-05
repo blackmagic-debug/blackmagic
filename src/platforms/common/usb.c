43e7910742f9e76ff14f314906cdeb4a5b66c2aa
dragonmux <git@dragonmux.network>
2023-01-05 22:08:12 +0000

    common/usb: Expanded the EP0 backing buffer to cater to the larger microsoft OS-specific descriptors

--- src/platforms/common/usb.c
+++ src/platforms/common/usb.c
@@ -31,14 +31,20 @@ usbd_device *usbdev = NULL;
 uint16_t usb_config;
 
 /* We need a special large control buffer for this device: */
-static uint8_t usbd_control_buffer[256];
+static uint8_t usbd_control_buffer[512];
+
+/*
+ * Please note, if you change the descriptors and any result exceeds this buffer size
+ * it will result in crashing behaviour when requested. Please adjust this buffer
+ * to fit your EP0 transactions.
+ */
 
 void blackmagic_usb_init(void)
 {
 	read_serial_number();
 
-	usbdev = usbd_init(&USB_DRIVER, &dev_desc, &config, usb_strings, ARRAY_LENGTH(usb_strings),
-		usbd_control_buffer, sizeof(usbd_control_buffer));
+	usbdev = usbd_init(&USB_DRIVER, &dev_desc, &config, usb_strings, ARRAY_LENGTH(usb_strings), usbd_control_buffer,
+		sizeof(usbd_control_buffer));
 
 	usbd_register_bos_descriptor(usbdev, &bos);
 	microsoft_os_register_descriptor_sets(usbdev, microsoft_os_descriptor_sets, DESCRIPTOR_SETS);
