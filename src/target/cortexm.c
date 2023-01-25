0889e75d8d5988257ef682e3ca691770dc684330
dragonmux <git@dragonmux.network>
2023-01-25 09:48:32 +0000

    imxrt: Begun implementing detection for the i.MXRT parts

--- src/target/cortexm.c
+++ src/target/cortexm.c
@@ -661,6 +661,7 @@ bool cortexm_probe(adiv5_access_port_s *ap)
 	switch (t->designer_code) {
 	case JEP106_MANUFACTURER_FREESCALE:
 		PROBE(kinetis_probe);
+		PROBE(imxrt_probe);
 		break;
 	case JEP106_MANUFACTURER_GIGADEVICE:
 		PROBE(gd32f1_probe);
