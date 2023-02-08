f969d2083ae9eabe4d3816cf2f4f73ec14740cee
dragonmux <git@dragonmux.network>
2023-02-08 07:05:21 +0000

    riscv_debug: Implemented logic for iterating and discovering DMs

--- src/target/riscv_debug.c
+++ src/target/riscv_debug.c
@@ -34,7 +34,13 @@
 #include "general.h"
 #include "riscv_debug.h"
 
-bool riscv_dmi_read(riscv_dmi_s *dmi, uint32_t address, uint32_t *value);
+#define RV_DM_CONTROL 0x10U
+#define RV_DM_STATUS  0x11U
+#define RV_DM_NEXT_DM 0x1dU
+
+static void riscv_dm_init(riscv_dm_s *dbg_module);
+static bool riscv_dmi_read(riscv_dmi_s *dmi, uint32_t address, uint32_t *value);
+static riscv_debug_version_e riscv_dm_version(uint32_t status);
 
 void riscv_dmi_init(riscv_dmi_s *const dmi)
 {
@@ -45,11 +51,73 @@ void riscv_dmi_init(riscv_dmi_s *const dmi)
 		DEBUG_INFO("RISC-V debug v0.11 not presently supported\n");
 		return;
 	}
+
+	/* Iterate through the possible DMs and probe implemented ones */
+	/* The first DM is always at base address 0 */
+	uint32_t base_addr = 0U;
+	do {
+		/* Read out the DM's status register */
+		uint32_t dm_status = 0;
+		if (!riscv_dmi_read(dmi, base_addr + RV_DM_STATUS, &dm_status)) {
+			/* If we fail to read the status register, abort */
+			break;
+		}
+		const riscv_debug_version_e dm_version = riscv_dm_version(dm_status);
+
+		if (dm_version != RISCV_DEBUG_UNIMPL) {
+			riscv_dm_s *dbg_module = calloc(1, sizeof(*dbg_module));
+			if (!dbg_module) { /* calloc failed: heap exhaustion */
+				DEBUG_WARN("calloc: failed in %s\n", __func__);
+				return;
+			}
+			/* Setup and try to discover the DM */
+			dbg_module->dmi_bus = dmi;
+			dbg_module->base = base_addr;
+			dbg_module->version = dm_version;
+			riscv_dm_init(dbg_module);
+			/* If we failed to discover any Harts, free the structure */
+			if (!dbg_module->ref_count)
+				free(dbg_module);
+		}
+
+		/* Read out the address of the next DM */
+		if (!riscv_dmi_read(dmi, base_addr + RV_DM_NEXT_DM, &base_addr)) {
+			/* If this fails then abort further scanning */
+			DEBUG_INFO("Error while reading the next DM base address\n");
+			break;
+		}
+		/* A new base address of 0 indicates this is the last one on the chain and we should stop. */
+	} while (base_addr != 0U);
 }
 
-bool riscv_dmi_read(riscv_dmi_s *const dmi, const uint32_t address, uint32_t *const value)
+static void riscv_dm_init(riscv_dm_s *const dbg_module)
+{
+	(void)dbg_module;
+}
+
+static bool riscv_dmi_read(riscv_dmi_s *const dmi, const uint32_t address, uint32_t *const value)
 {
 	return dmi->read(dmi, address, value);
+}
+
+static riscv_debug_version_e riscv_dm_version(const uint32_t status)
+{
+	switch (status & RV_STATUS_VERSION_MASK) {
+	case 0:
+		return RISCV_DEBUG_UNIMPL;
+	case 1:
+		DEBUG_INFO("RISC-V debug v0.11 DM\n");
+		return RISCV_DEBUG_0_11;
+	case 2:
+		DEBUG_INFO("RISC-V debug v0.13 DM\n");
+		return RISCV_DEBUG_0_13;
+	case 3:
+		DEBUG_INFO("RISC-V debug v1.0 DM\n");
+		return RISCV_DEBUG_1_0;
+	}
+	DEBUG_INFO(
+		"Please report part with unknown RISC-V debug DM version %x\n", (uint8_t)(status & RV_STATUS_VERSION_MASK));
+	return RISCV_DEBUG_UNKNOWN;
 }
 
 static inline void riscv_dmi_ref(riscv_dmi_s *const dmi)
