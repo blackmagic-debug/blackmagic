9ccae8da367d3f2dabd06329f9dc71ee2b2bad5b
dragonmux <git@dragonmux.network>
2023-02-16 00:44:50 +0000

    hex_utils: Made the hex_digit and unhex_digit functions available to the rest of the firmware

--- src/include/hex_utils.h
+++ src/include/hex_utils.h
@@ -3,6 +3,8 @@
  *
  * Copyright (C) 2011  Black Sphere Technologies Ltd.
  * Written by Gareth McMullin <gareth@blacksphere.co.nz>
+ * Copyright (C) 2023 1BitSquared <info@1bitsquared.com>
+ * Modified by Rachel Mant <git@dragonmux.network>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,9 +23,13 @@
 #ifndef INCLUDE_HEX_UTILS_H
 #define INCLUDE_HEX_UTILS_H
 
+#include <stdint.h>
 #include <stddef.h>
 
 char *hexify(char *hex, const void *buf, size_t size);
 char *unhexify(void *buf, const char *hex, size_t size);
+
+char hex_digit(uint8_t value);
+uint8_t unhex_digit(char hex);
 
 #endif /* INCLUDE_HEX_UTILS_H */
