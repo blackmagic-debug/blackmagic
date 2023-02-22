6f4b9bce694cf6c1107b5a0e043d0ca323bfc86e
dragonmux <git@dragonmux.network>
2023-02-22 02:05:16 +0000

    hosted/cmsis_dap: Updated the file headers

--- src/platforms/hosted/dap.c
+++ src/platforms/hosted/dap.c
@@ -1,5 +1,7 @@
 /*
  * Copyright (c) 2013-2015, Alex Taradov <alex@taradov.com>
+ * Copyright (C) 2023 1BitSquared <info@1bitsquared.com>
+ * Modified by Rachel Mant <git@dragonmux.network>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
