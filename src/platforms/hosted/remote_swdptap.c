6912994d4c084c0842eb889a729329b141fadeeb
dragonmux <git@dragonmux.network>
2023-03-16 09:21:27 +0000

    hosted/remote_swdptap: Cleaned up the naming of remote_swd_seq_out

--- src/platforms/hosted/remote_swdptap.c
+++ src/platforms/hosted/remote_swdptap.c
@@ -33,7 +33,7 @@
 
 static bool remote_swd_seq_in_parity(uint32_t *res, size_t clock_cycles);
 static uint32_t remote_swd_seq_in(size_t clock_cycles);
-static void swdptap_seq_out(uint32_t tms_states, size_t clock_cycles);
+static void remote_swd_seq_out(uint32_t tms_states, size_t clock_cycles);
 static void swdptap_seq_out_parity(uint32_t tms_states, size_t clock_cycles);
 
 bool remote_swdptap_init(void)
@@ -50,7 +50,7 @@ bool remote_swdptap_init(void)
 
 	swd_proc.seq_in = remote_swd_seq_in;
 	swd_proc.seq_in_parity = remote_swd_seq_in_parity;
-	swd_proc.seq_out = swdptap_seq_out;
+	swd_proc.seq_out = remote_swd_seq_out;
 	swd_proc.seq_out_parity = swdptap_seq_out_parity;
 	return true;
 }
@@ -91,17 +91,17 @@ static uint32_t remote_swd_seq_in(size_t clock_cycles)
 	return res;
 }
 
-static void swdptap_seq_out(uint32_t tms_states, size_t clock_cycles)
+static void remote_swd_seq_out(uint32_t tms_states, size_t clock_cycles)
 {
 	char buffer[REMOTE_MAX_MSG_SIZE];
 
-	DEBUG_PROBE("swdptap_seq_out %zu clock_cycles: %08" PRIx32 "\n", clock_cycles, tms_states);
+	DEBUG_PROBE("%s %zu clock_cycles: %08" PRIx32 "\n", __func__, clock_cycles, tms_states);
 	int length = sprintf(buffer, REMOTE_SWDP_OUT_STR, clock_cycles, tms_states);
 	platform_buffer_write(buffer, length);
 
 	length = platform_buffer_read(buffer, REMOTE_MAX_MSG_SIZE);
 	if (length < 1 || buffer[0] == REMOTE_RESP_ERR) {
-		DEBUG_WARN("swdptap_seq_out failed, error %s\n", length ? buffer + 1 : "short response");
+		DEBUG_WARN("%s failed, error %s\n", __func__, length ? buffer + 1 : "short response");
 		exit(-1);
 	}
 }
