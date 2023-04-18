67a949b1d22ff7ed633778e37505a0499ab29a93
Sascha Silbe <sascha-pgp@silbe.org>
2023-04-18 15:49:01 +0200

    command: reset: add optional pulse length parameter
    For some targets the very short pulse resulting from asserting nRST
    and deasserting it without delay may not be enough for a reliable
    reset (or possibly any at all). Add an optional parameter that allows
    asserting nRST for a given number of milliseconds.

--- src/command.c
+++ src/command.c
@@ -91,7 +91,7 @@ const command_s cmd_list[] = {
 	{"morse", cmd_morse, "Display morse error message"},
 	{"halt_timeout", cmd_halt_timeout, "Timeout (ms) to wait until Cortex-M is halted: (Default 2000)"},
 	{"connect_rst", cmd_connect_reset, "Configure connect under reset: (enable|disable)"},
-	{"reset", cmd_reset, "Pulse the nRST line - disconnects target"},
+	{"reset", cmd_reset, "Pulse the nRST line - disconnects target: (pulse_len_ms, default 0)"},
 	{"tdi_low_reset", cmd_tdi_low_reset,
 		"Pulse nRST with TDI set low to attempt to wake certain targets up (eg LPC82x)"},
 #ifdef PLATFORM_HAS_POWER_SWITCH
@@ -454,10 +454,12 @@ static bool cmd_halt_timeout(target_s *t, int argc, const char **argv)
 static bool cmd_reset(target_s *t, int argc, const char **argv)
 {
 	(void)t;
-	(void)argc;
-	(void)argv;
+	uint32_t pulse_len_ms = 0;
+	if (argc > 1)
+		pulse_len_ms = strtoul(argv[1], NULL, 0);
 	target_list_free();
 	platform_nrst_set_val(true);
+	platform_delay(pulse_len_ms);
 	platform_nrst_set_val(false);
 	return true;
 }
