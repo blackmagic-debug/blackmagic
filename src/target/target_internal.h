27e5402691e368bc87176cf104038282aee0ae76
dragonmux <git@dragonmux.network>
2023-04-28 21:32:41 +0100

    target_internal: Fixed the clang-tidy warnings about parameter naming

--- src/target/target_internal.h
+++ src/target/target_internal.h
@@ -40,10 +40,10 @@ struct target_ram {
 
 typedef struct target_flash target_flash_s;
 
-typedef bool (*flash_prepare_func)(target_flash_s *f);
-typedef bool (*flash_erase_func)(target_flash_s *f, target_addr_t addr, size_t len);
-typedef bool (*flash_write_func)(target_flash_s *f, target_addr_t dest, const void *src, size_t len);
-typedef bool (*flash_done_func)(target_flash_s *f);
+typedef bool (*flash_prepare_func)(target_flash_s *flash);
+typedef bool (*flash_erase_func)(target_flash_s *flash, target_addr_t addr, size_t len);
+typedef bool (*flash_write_func)(target_flash_s *flash, target_addr_t dest, const void *src, size_t len);
+typedef bool (*flash_done_func)(target_flash_s *flash);
 
 struct target_flash {
 	target_s *t;                 /* Target this flash is attached to */
@@ -65,7 +65,7 @@ struct target_flash {
 	target_flash_s *next;        /* Next flash in list */
 };
 
-typedef bool (*cmd_handler_fn)(target_s *t, int argc, const char **argv);
+typedef bool (*cmd_handler_fn)(target_s *target, int argc, const char **argv);
 
 typedef struct command {
 	const char *cmd;
@@ -98,40 +98,40 @@ struct target {
 	target_controller_s *tc;
 
 	/* Attach/Detach functions */
-	bool (*attach)(target_s *t);
-	void (*detach)(target_s *t);
-	bool (*check_error)(target_s *t);
+	bool (*attach)(target_s *target);
+	void (*detach)(target_s *target);
+	bool (*check_error)(target_s *target);
 
 	/* Memory access functions */
-	void (*mem_read)(target_s *t, void *dest, target_addr_t src, size_t len);
-	void (*mem_write)(target_s *t, target_addr_t dest, const void *src, size_t len);
+	void (*mem_read)(target_s *target, void *dest, target_addr_t src, size_t len);
+	void (*mem_write)(target_s *target, target_addr_t dest, const void *src, size_t len);
 
 	/* Register access functions */
 	size_t regs_size;
-	const char *(*regs_description)(target_s *t);
-	void (*regs_read)(target_s *t, void *data);
-	void (*regs_write)(target_s *t, const void *data);
-	ssize_t (*reg_read)(target_s *t, int reg, void *data, size_t max);
-	ssize_t (*reg_write)(target_s *t, int reg, const void *data, size_t size);
+	const char *(*regs_description)(target_s *target);
+	void (*regs_read)(target_s *target, void *data);
+	void (*regs_write)(target_s *target, const void *data);
+	ssize_t (*reg_read)(target_s *target, int reg, void *data, size_t max);
+	ssize_t (*reg_write)(target_s *target, int reg, const void *data, size_t size);
 
 	/* Halt/resume functions */
-	void (*reset)(target_s *t);
-	void (*extended_reset)(target_s *t);
-	void (*halt_request)(target_s *t);
-	target_halt_reason_e (*halt_poll)(target_s *t, target_addr_t *watch);
-	void (*halt_resume)(target_s *t, bool step);
+	void (*reset)(target_s *target);
+	void (*extended_reset)(target_s *target);
+	void (*halt_request)(target_s *target);
+	target_halt_reason_e (*halt_poll)(target_s *target, target_addr_t *watch);
+	void (*halt_resume)(target_s *target, bool step);
 
 	/* Break-/watchpoint functions */
-	int (*breakwatch_set)(target_s *t, breakwatch_s *);
-	int (*breakwatch_clear)(target_s *t, breakwatch_s *);
+	int (*breakwatch_set)(target_s *target, breakwatch_s *);
+	int (*breakwatch_clear)(target_s *target, breakwatch_s *);
 	breakwatch_s *bw_list;
 
 	/* Recovery functions */
-	bool (*mass_erase)(target_s *t);
+	bool (*mass_erase)(target_s *target);
 
 	/* Flash functions */
-	bool (*enter_flash_mode)(target_s *t);
-	bool (*exit_flash_mode)(target_s *t);
+	bool (*enter_flash_mode)(target_s *target);
+	bool (*exit_flash_mode)(target_s *target);
 	bool flash_mode;
 
 	/* Target-defined options */
@@ -173,39 +173,39 @@ struct target {
 };
 
 void target_print_progress(platform_timeout_s *timeout);
-void target_ram_map_free(target_s *t);
-void target_flash_map_free(target_s *t);
-void target_mem_map_free(target_s *t);
-void target_add_commands(target_s *t, const command_s *cmds, const char *name);
-void target_add_ram(target_s *t, target_addr_t start, uint32_t len);
-void target_add_flash(target_s *t, target_flash_s *f);
+void target_ram_map_free(target_s *target);
+void target_flash_map_free(target_s *target);
+void target_mem_map_free(target_s *target);
+void target_add_commands(target_s *target, const command_s *cmds, const char *name);
+void target_add_ram(target_s *target, target_addr_t start, uint32_t len);
+void target_add_flash(target_s *target, target_flash_s *flash);
 
-target_flash_s *target_flash_for_addr(target_s *t, uint32_t addr);
+target_flash_s *target_flash_for_addr(target_s *target, uint32_t addr);
 
 /* Convenience function for MMIO access */
-uint32_t target_mem_read32(target_s *t, uint32_t addr);
-uint16_t target_mem_read16(target_s *t, uint32_t addr);
-uint8_t target_mem_read8(target_s *t, uint32_t addr);
-void target_mem_write32(target_s *t, uint32_t addr, uint32_t value);
-void target_mem_write16(target_s *t, uint32_t addr, uint16_t value);
-void target_mem_write8(target_s *t, uint32_t addr, uint8_t value);
-bool target_check_error(target_s *t);
+uint32_t target_mem_read32(target_s *target, uint32_t addr);
+uint16_t target_mem_read16(target_s *target, uint32_t addr);
+uint8_t target_mem_read8(target_s *target, uint32_t addr);
+void target_mem_write32(target_s *target, uint32_t addr, uint32_t value);
+void target_mem_write16(target_s *target, uint32_t addr, uint16_t value);
+void target_mem_write8(target_s *target, uint32_t addr, uint8_t value);
+bool target_check_error(target_s *target);
 
 /* Access to host controller interface */
-void tc_printf(target_s *t, const char *fmt, ...);
+void tc_printf(target_s *target, const char *fmt, ...);
 
 /* Interface to host system calls */
 int tc_open(target_s *, target_addr_t path, size_t plen, target_open_flags_e flags, mode_t mode);
-int tc_close(target_s *t, int fd);
-int tc_read(target_s *t, int fd, target_addr_t buf, unsigned int count);
-int tc_write(target_s *t, int fd, target_addr_t buf, unsigned int count);
-long tc_lseek(target_s *t, int fd, long offset, target_seek_flag_e flag);
-int tc_rename(target_s *t, target_addr_t oldpath, size_t oldlen, target_addr_t newpath, size_t newlen);
-int tc_unlink(target_s *t, target_addr_t path, size_t plen);
-int tc_stat(target_s *t, target_addr_t path, size_t plen, target_addr_t buf);
-int tc_fstat(target_s *t, int fd, target_addr_t buf);
-int tc_gettimeofday(target_s *t, target_addr_t tv, target_addr_t tz);
-int tc_isatty(target_s *t, int fd);
-int tc_system(target_s *t, target_addr_t cmd, size_t cmdlen);
+int tc_close(target_s *target, int fd);
+int tc_read(target_s *target, int fd, target_addr_t buf, unsigned int count);
+int tc_write(target_s *target, int fd, target_addr_t buf, unsigned int count);
+long tc_lseek(target_s *target, int fd, long offset, target_seek_flag_e flag);
+int tc_rename(target_s *target, target_addr_t oldpath, size_t oldlen, target_addr_t newpath, size_t newlen);
+int tc_unlink(target_s *target, target_addr_t path, size_t plen);
+int tc_stat(target_s *target, target_addr_t path, size_t plen, target_addr_t buf);
+int tc_fstat(target_s *target, int fd, target_addr_t buf);
+int tc_gettimeofday(target_s *target, target_addr_t tv, target_addr_t tz);
+int tc_isatty(target_s *target, int fd);
+int tc_system(target_s *target, target_addr_t cmd, size_t cmdlen);
 
 #endif /* TARGET_TARGET_INTERNAL_H */
