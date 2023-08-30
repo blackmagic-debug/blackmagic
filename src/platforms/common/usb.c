3eabf98c3625c1476a607b1f466c9e7f55184184
Kamil Rogozinski <kamil@convergedfields.com>
2023-08-30 22:24:29 +0800

    native/platform: flash error led using morse subsystem to indicate no USB host present.

--- src/platforms/common/usb.c
+++ src/platforms/common/usb.c
@@ -40,6 +40,15 @@ static uint8_t usbd_control_buffer[512];
  * to fit your EP0 transactions.
  */
 
+static bool usb_config_updated = true;
+
+static void usb_config_set_updated(usbd_device *const dev, const uint16_t value)
+{
+	(void)dev;
+	(void)value;
+	usb_config_updated = true;
+}
+
 void blackmagic_usb_init(void)
 {
 	read_serial_number();
@@ -51,6 +60,7 @@ void blackmagic_usb_init(void)
 	microsoft_os_register_descriptor_sets(usbdev, microsoft_os_descriptor_sets, DESCRIPTOR_SETS);
 	usbd_register_set_config_callback(usbdev, usb_serial_set_config);
 	usbd_register_set_config_callback(usbdev, dfu_set_config);
+	usbd_register_set_config_callback(usbdev, usb_config_set_updated);
 
 	nvic_set_priority(USB_IRQ, IRQ_PRI_USB);
 	nvic_enable_irq(USB_IRQ);
@@ -64,4 +74,14 @@ uint16_t usb_get_config(void)
 void USB_ISR(void)
 {
 	usbd_poll(usbdev);
+}
+
+bool usb_config_is_updated(void)
+{
+	return usb_config_updated;
+}
+
+void usb_config_clear_updated(void)
+{
+	usb_config_updated = false;
 }
