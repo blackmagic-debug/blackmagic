41bfd64e7991e07834cad49e026947970456b701
Aki Van Ness <aki@lethalbit.net>
2023-10-29 15:16:26 -0700

    jtag_scan: updated JTAG_MAX_IR_LEN to be the max bit-length of the IR storage, even though it's not large enough for all Xilinx devices

--- src/target/jtag_scan.h
+++ src/target/jtag_scan.h
@@ -27,7 +27,7 @@
 #include "jtagtap.h"
 
 #define JTAG_MAX_DEVS   32U
-#define JTAG_MAX_IR_LEN 38U
+#define JTAG_MAX_IR_LEN 32U /* NOTE: This is not long enough for all Xilinx devices */
 
 typedef struct jtag_dev {
 	uint32_t jd_idcode;
