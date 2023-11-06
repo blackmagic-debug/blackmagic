0c31f1268adb2732a9ef976804927454a383adf0
ALTracer <tolstov_den@mail.ru>
2023-11-06 20:40:01 +0300

    gdb_packet: Mark gdb_putpacket_f() as printf-like (for -Wformat)

--- src/include/gdb_packet.h
+++ src/include/gdb_packet.h
@@ -39,7 +39,7 @@ size_t gdb_getpacket(char *packet, size_t size);
 void gdb_putpacket(const char *packet, size_t size);
 void gdb_putpacket2(const char *packet1, size_t size1, const char *packet2, size_t size2);
 #define gdb_putpacketz(packet) gdb_putpacket((packet), strlen(packet))
-void gdb_putpacket_f(const char *packet, ...);
+void gdb_putpacket_f(const char *packet, ...) __attribute__((format(printf, 1, 2)));
 void gdb_put_notification(const char *packet, size_t size);
 #define gdb_put_notificationz(packet) gdb_put_notification((packet), strlen(packet))
 
