79dcd73ee7d44c9e58db791efce45ca393b34b3e
dragonmux <git@dragonmux.network>
2023-12-14 07:54:47 +0000

    renesas_rz: Implemented setup for SPI Flash transactions on the Multi I/O Bus Controller

--- src/target/renesas_rz.c
+++ src/target/renesas_rz.c
@@ -35,6 +35,8 @@
 #include "general.h"
 #include "target.h"
 #include "target_internal.h"
+#include "spi.h"
+#include "sfdp.h"
 
 /*
  * Part numbering scheme
@@ -92,14 +94,33 @@
 #define RENESAS_BSCAN_BSID_RZ_A1LC   0x082f4447U
 
 /* SPI Multi I/O Bus Controller registers, from R01UH0437EJ0600 §17.4, pg739 (17-4) */
-#define RENESAS_MULTI_IO_SPI_BASE        0x3fefa000U
-#define RENESAS_MULTI_IO_SPI_COMMON_CTRL (RENESAS_MULTI_IO_SPI_BASE + 0x000U)
-#define RENESAS_MULTI_IO_SPI_READ_CTRL   (RENESAS_MULTI_IO_SPI_BASE + 0x00cU)
-#define RENESAS_MULTI_IO_SPI_MODE_STATUS (RENESAS_MULTI_IO_SPI_BASE + 0x048U)
+#define RENESAS_MULTI_IO_SPI_BASE             0x3fefa000U
+#define RENESAS_MULTI_IO_SPI_COMMON_CTRL      (RENESAS_MULTI_IO_SPI_BASE + 0x000U)
+#define RENESAS_MULTI_IO_SPI_READ_CTRL        (RENESAS_MULTI_IO_SPI_BASE + 0x00cU)
+#define RENESAS_MULTI_IO_SPI_MODE_CTRL        (RENESAS_MULTI_IO_SPI_BASE + 0x020U)
+#define RENESAS_MULTI_IO_SPI_MODE_CMD         (RENESAS_MULTI_IO_SPI_BASE + 0x024U)
+#define RENESAS_MULTI_IO_SPI_MODE_ADDR        (RENESAS_MULTI_IO_SPI_BASE + 0x028U)
+#define RENESAS_MULTI_IO_SPI_MODE_DUMMY_DATA  (RENESAS_MULTI_IO_SPI_BASE + 0x02cU)
+#define RENESAS_MULTI_IO_SPI_MODE_XFER_CONFIG (RENESAS_MULTI_IO_SPI_BASE + 0x030U)
+#define RENESAS_MULTI_IO_SPI_MODE_READ_DATA   (RENESAS_MULTI_IO_SPI_BASE + 0x038U)
+#define RENESAS_MULTI_IO_SPI_MODE_WRITE_DATA  (RENESAS_MULTI_IO_SPI_BASE + 0x040U)
+#define RENESAS_MULTI_IO_SPI_MODE_STATUS      (RENESAS_MULTI_IO_SPI_BASE + 0x048U)
 
-#define RENESAS_MULTI_IO_SPI_COMMON_CTRL_MODE_SPI      (1U << 31U)
-#define RENESAS_MULTI_IO_SPI_READ_CTRL_CS_UNSELECT     (1U << 24U)
-#define RENESAS_MULTI_IO_SPI_MODE_STATUS_XFER_COMPLETE (1U << 0U)
+#define RENESAS_MULTI_IO_SPI_COMMON_CTRL_MODE_SPI             (1U << 31U)
+#define RENESAS_MULTI_IO_SPI_READ_CTRL_CS_UNSELECT            (1U << 24U)
+#define RENESAS_MULTI_IO_SPI_MODE_CTRL_CS_HOLD                (1U << 8U)
+#define RENESAS_MULTI_IO_SPI_MODE_CTRL_READ_ENABLE            (1U << 2U)
+#define RENESAS_MULTI_IO_SPI_MODE_CTRL_WRITE_ENABLE           (1U << 1U)
+#define RENESAS_MULTI_IO_SPI_MODE_CTRL_RUN_XFER               (1U << 0U)
+#define RENESAS_MULTI_IO_SPI_MODE_CMD_SHIFT                   16U
+#define RENESAS_MULTI_IO_SPI_MODE_CMD_MASK                    0x00ff0000U
+#define RENESAS_MULTI_IO_SPI_MODE_XFER_CONFIG_CMD             (1U << 14U)
+#define RENESAS_MULTI_IO_SPI_MODE_XFER_CONFIG_ADDR_NONE       (0x0U << 8U)
+#define RENESAS_MULTI_IO_SPI_MODE_XFER_CONFIG_ADDR_3B         (0x7U << 8U)
+#define RENESAS_MULTI_IO_SPI_MODE_XFER_CONFIG_ADDR_4B         (0xfU << 8U)
+#define RENESAS_MULTI_IO_SPI_MODE_XFER_CONFIG_DUMMY_SHIFT     4U
+#define RENESAS_MULTI_IO_SPI_MODE_XFER_CONFIG_DATA_XFER_SHIFT 0U
+#define RENESAS_MULTI_IO_SPI_MODE_STATUS_XFER_COMPLETE        (1U << 0U)
 
 /* This is the part number from the ROM table of a R7S721030 and is a guess */
 #define ID_RZ_A1LU 0x012U
@@ -107,6 +128,7 @@
 static const char *renesas_rz_part_name(uint32_t part_id);
 static bool renesas_rz_flash_prepare(target_s *target);
 static bool renesas_rz_flash_resume(target_s *target);
+static void renesas_rz_spi_read(target_s *target, uint16_t command, target_addr_t address, void *buffer, size_t length);
 
 static void renesas_rz_add_flash(target_s *const target)
 {
@@ -114,6 +136,8 @@ static void renesas_rz_add_flash(target_s *const target)
 	target->exit_flash_mode = renesas_rz_flash_resume;
 
 	renesas_rz_flash_prepare(target);
+	spi_parameters_s parameters;
+	sfdp_read_parameters(target, &parameters, renesas_rz_spi_read);
 	renesas_rz_flash_resume(target);
 }
 
@@ -175,4 +199,55 @@ static bool renesas_rz_flash_resume(target_s *const target)
 	target_mem_write32(target, RENESAS_MULTI_IO_SPI_COMMON_CTRL,
 		target_mem_read32(target, RENESAS_MULTI_IO_SPI_COMMON_CTRL) & ~RENESAS_MULTI_IO_SPI_COMMON_CTRL_MODE_SPI);
 	return true;
+}
+
+static uint32_t renesas_rz_spi_setup_xfer(
+	target_s *const target, const uint16_t command, const target_addr_t address, const size_t length)
+{
+	/* Set up the command byte, dummy bytes and address for the transfer */
+	const uint8_t opcode = command & SPI_FLASH_OPCODE_MASK;
+	target_mem_write32(target, RENESAS_MULTI_IO_SPI_MODE_CMD, opcode << RENESAS_MULTI_IO_SPI_MODE_CMD_SHIFT);
+	target_mem_write32(target, RENESAS_MULTI_IO_SPI_MODE_ADDR, address);
+	target_mem_write32(target, RENESAS_MULTI_IO_SPI_MODE_DUMMY_DATA, 0U);
+
+	/* Set up the phases that need to be enabled for the transfer */
+	uint32_t config = RENESAS_MULTI_IO_SPI_MODE_XFER_CONFIG_CMD;
+	if ((command & SPI_FLASH_OPCODE_MODE_MASK) == SPI_FLASH_OPCODE_3B_ADDR)
+		config |= RENESAS_MULTI_IO_SPI_MODE_XFER_CONFIG_ADDR_3B;
+	/* If we need to insert any dummy byte cycles into the transaction */
+	const uint8_t dummy_bytes = (command & SPI_FLASH_DUMMY_MASK) >> SPI_FLASH_DUMMY_SHIFT;
+	if (dummy_bytes) {
+		/*
+		 * Create a bit mask for how much of the dummy data register to send to generate enough cycles.
+		 * This will start out right aligned - eg, for 3 dummy cycles, 0b0111, so we need to then
+		 * left align this within the nibble by shifting by 4 - dummy_bytes, producing a value like 0b1110.
+		 * Finally we then shift this up into place within the config value.
+		 * NB: This will not work for any more than 4 dummy byte cycles.
+		 */
+		config |= (((1U << dummy_bytes) - 1U) << (4U - dummy_bytes))
+			<< RENESAS_MULTI_IO_SPI_MODE_XFER_CONFIG_DUMMY_SHIFT;
+	}
+	/* Create a bit mask for the first part of the required data transfer, same process as with the dummy bytes */
+	const uint8_t initial_bytes = MIN(4U, length);
+	config |= (((1U << initial_bytes) - 1U) << (4U - initial_bytes))
+		<< RENESAS_MULTI_IO_SPI_MODE_XFER_CONFIG_DATA_XFER_SHIFT;
+	target_mem_write32(target, RENESAS_MULTI_IO_SPI_MODE_XFER_CONFIG, config);
+
+	/* If the transfer has no data associated with it, do not enable either transfer direction */
+	if (!length)
+		return 0;
+	/*
+	 * Convert the transaction direction into a control register direction setting and
+	 * configure if we need ~CS held at the end of the transfer
+	 */
+	return ((command & SPI_FLASH_DATA_MASK) == SPI_FLASH_DATA_IN ? RENESAS_MULTI_IO_SPI_MODE_CTRL_READ_ENABLE :
+																   RENESAS_MULTI_IO_SPI_MODE_CTRL_WRITE_ENABLE) |
+		(length > 4U ? RENESAS_MULTI_IO_SPI_MODE_CTRL_CS_HOLD : 0U) | RENESAS_MULTI_IO_SPI_MODE_CTRL_RUN_XFER;
+}
+
+static void renesas_rz_spi_read(target_s *const target, const uint16_t command, const target_addr_t address,
+	void *const buffer, const size_t length)
+{
+	/* Set up the transaction */
+	renesas_rz_spi_setup_xfer(target, command, address, length);
 }
