cd96de904d04570b9bbbe33a877d9abaadc20e3a
dragonmux <git@dragonmux.network>
2023-12-21 15:39:11 +0000

    semihosting: Moved the Cortex-M implementation out into a new file

--- src/target/semihosting.h
+++ src/target/semihosting.h
@@ -1,7 +1,10 @@
 /*
  * This file is part of the Black Magic Debug project.
  *
- * Copyright (C) 2022  Black Sphere Technologies Ltd.
+ * Copyright (C) 2022 Black Sphere Technologies Ltd.
+ * Copyright (C) 2023 1BitSquared <info@1bitsquared.com>
+ * Written by Perigoso <git@dragonmux.network>
+ * Modified by Rachel Mant <git@dragonmux.network>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,43 +23,8 @@
 #ifndef TARGET_SEMIHOSTING_H
 #define TARGET_SEMIHOSTING_H
 
-/* Semihosting support */
+#include "general.h"
 
-/*
- * If the target wants to read the special filename ":semihosting-features"
- * to know what semihosting features are supported, it's easiest to create
- * that file on the host in the directory where gdb runs,
- * or, if using pc-hosted, where blackmagic_hosted runs.
- *
- * $ echo -e 'SHFB\x03' > ":semihosting-features"
- * $ chmod 0444 ":semihosting-features"
- */
-
-/* ARM Semihosting syscall numbers, from "Semihosting for AArch32 and AArch64 Version 3.0" */
-
-#define SEMIHOSTING_SYS_CLOCK         0x10U
-#define SEMIHOSTING_SYS_CLOSE         0x02U
-#define SEMIHOSTING_SYS_ELAPSED       0x30U
-#define SEMIHOSTING_SYS_ERRNO         0x13U
-#define SEMIHOSTING_SYS_EXIT          0x18U
-#define SEMIHOSTING_SYS_EXIT_EXTENDED 0x20U
-#define SEMIHOSTING_SYS_FLEN          0x0cU
-#define SEMIHOSTING_SYS_GET_CMDLINE   0x15U
-#define SEMIHOSTING_SYS_HEAPINFO      0x16U
-#define SEMIHOSTING_SYS_ISERROR       0x08U
-#define SEMIHOSTING_SYS_ISTTY         0x09U
-#define SEMIHOSTING_SYS_OPEN          0x01U
-#define SEMIHOSTING_SYS_READ          0x06U
-#define SEMIHOSTING_SYS_READC         0x07U
-#define SEMIHOSTING_SYS_REMOVE        0x0eU
-#define SEMIHOSTING_SYS_RENAME        0x0fU
-#define SEMIHOSTING_SYS_SEEK          0x0aU
-#define SEMIHOSTING_SYS_SYSTEM        0x12U
-#define SEMIHOSTING_SYS_TICKFREQ      0x31U
-#define SEMIHOSTING_SYS_TIME          0x11U
-#define SEMIHOSTING_SYS_TMPNAM        0x0dU
-#define SEMIHOSTING_SYS_WRITE         0x05U
-#define SEMIHOSTING_SYS_WRITEC        0x03U
-#define SEMIHOSTING_SYS_WRITE0        0x04U
+int cortexm_hostio_request(target_s *target);
 
 #endif /* TARGET_SEMIHOSTING_H */
