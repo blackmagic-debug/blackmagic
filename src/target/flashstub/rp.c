00c9b0807c3f0216f2d4e440ef627e565b966251
dragonmux <git@dragonmux.network>
2024-01-29 11:04:10 +0000

    flashstub/rp: Made use of a variety of techniques to reduce the size of the stub down and improve correctness

--- src/target/flashstub/rp.c
+++ src/target/flashstub/rp.c
@@ -29,41 +29,77 @@
 #define SPI_FLASH_OPCODE_MODE_MASK 0x0800U
 #define SPI_FLASH_OPCODE_3B_ADDR   (1U << 11U)
 
-// rp.c
-#define RP_SSI_BASE_ADDR 0x18000000U
-#define RP_SSI_CTRL1     *((volatile uint32_t *)(RP_SSI_BASE_ADDR + 0x04U))
-#define RP_SSI_DR0       *((volatile uint32_t *)(RP_SSI_BASE_ADDR + 0x60U))
-#define RP_SSI_SR        *((volatile uint32_t *)(RP_SSI_BASE_ADDR + 0x28U))
-#define RP_SSI_SR_TFE    (1U << 2U)
+/* SSI peripheral registers */
+typedef struct ssi {
+	volatile uint32_t ctrl0;
+	volatile uint32_t ctrl1;
+	/* These next registers aren't actually reserved, we just don't care about them */
+	uint32_t reserved1[8U];
+	const volatile uint32_t status;
+	/* Not all of these are reserved, but we don't care about them */
+	uint32_t reserved2[13U];
+	volatile uint32_t data;
+	/* We don't bother defining the rest of the registers as they're not important to us */
+} ssi_s;
 
+/* QSPI GPIO bank peripheral registers */
+typedef struct gpio_qspi {
+	const volatile uint32_t sclk_status;
+	volatile uint32_t sclk_ctrl;
+	const volatile uint32_t cs_status;
+	volatile uint32_t cs_ctrl;
+	/* We don't bother defining the rest of the registers as they're not important to us */
+} gpio_qspi_s;
+
+/* SSI peripheral base address and register bit definitions */
+#define RP_SSI_BASE_ADDR            0x18000000U
+#define RP_SSI_STATUS_FX_FIFO_EMPTY (1U << 2U)
+
+/* QSPI GPIO peripheral base address and register bit definitions */
 #define RP_GPIO_QSPI_BASE_ADDR     0x40018000U
-#define RP_GPIO_QSPI_CS_CTRL       *((volatile uint32_t *)(RP_GPIO_QSPI_BASE_ADDR + 0x0cU))
 #define RP_GPIO_QSPI_CS_DRIVE_MASK 0x00000300U
-
 #define RP_GPIO_QSPI_CS_DRIVE_LOW  (2U << 8U)
 #define RP_GPIO_QSPI_CS_DRIVE_HIGH (3U << 8U)
 
-#define SPI_CHIP_SELECT(state) RP_GPIO_QSPI_CS_CTRL = (RP_GPIO_QSPI_CS_CTRL & ~RP_GPIO_QSPI_CS_DRIVE_MASK) | state
+/* Define the controllers so we can access them */
+static ssi_s *const ssi = (ssi_s *)RP_SSI_BASE_ADDR;
+static gpio_qspi_s *const gpio_qspi = (gpio_qspi_s *)RP_GPIO_QSPI_BASE_ADDR;
 
-static __attribute__((always_inline)) uint8_t rp_spi_xfer_data(const uint8_t data)
+#define SPI_CHIP_SELECT(state) gpio_qspi->cs_ctrl = (gpio_qspi->cs_ctrl & ~RP_GPIO_QSPI_CS_DRIVE_MASK) | state
+
+static uint8_t rp_spi_xfer_data(const uint8_t data);
+
+void __attribute__((naked, used, section(".entry")))
+rp_flash_write_stub(const uint16_t command, const uint32_t dest, const uint8_t *const src, const uint32_t length)
 {
-	RP_SSI_DR0 = data;
-	while (!(RP_SSI_SR & RP_SSI_SR_TFE))
-		asm volatile("nop");
-
-	return RP_SSI_DR0 & 0xffU;
+	/* Create a stack for our own sanity */
+	__asm__("ldr r4, =#0x20042000\n"
+			"mov sp, r4\n"
+			"b rp_flash_write\n");
 }
 
-static __attribute__((always_inline)) void rp_spi_setup_xfer(
-	const uint16_t command, const size_t address, const size_t length)
+static uint8_t rp_spi_xfer_data(const uint8_t data)
 {
-	RP_SSI_CTRL1 = length;
+	/* Initiate the 8-bit transfer */
+	ssi->data = data;
+	/* Wait for it to complete */
+	while (!(ssi->status & RP_SSI_STATUS_FX_FIFO_EMPTY))
+		continue;
+	/* Then read the result so the FIFO doesn't wind up filled */
+	return ssi->data & 0xffU;
+}
+
+static __attribute__((used, section(".entry"))) void rp_flash_write(
+	const uint16_t command, const uint32_t address, const uint8_t *const src, const uint32_t length)
+{
+	/* Configure the controller, and select the Flash */
+	ssi->ctrl1 = length;
 	SPI_CHIP_SELECT(RP_GPIO_QSPI_CS_DRIVE_LOW);
 
-	const uint8_t opcode = command & SPI_FLASH_OPCODE_MASK;
-	rp_spi_xfer_data(opcode);
+	rp_spi_xfer_data((uint8_t)(command & SPI_FLASH_OPCODE_MASK));
 
 	if ((command & SPI_FLASH_OPCODE_MODE_MASK) == SPI_FLASH_OPCODE_3B_ADDR) {
+		/* For each byte sent here, we have to manually clean up from the controller with a read */
 		rp_spi_xfer_data((address >> 16U) & 0xffU);
 		rp_spi_xfer_data((address >> 8U) & 0xffU);
 		rp_spi_xfer_data(address & 0xffU);
@@ -71,18 +107,15 @@ static __attribute__((always_inline)) void rp_spi_setup_xfer(
 
 	const size_t inter_length = (command & SPI_FLASH_DUMMY_MASK) >> SPI_FLASH_DUMMY_SHIFT;
 	for (size_t i = 0; i < inter_length; ++i)
+		/* For each byte sent here, we have to manually clean up from the controller with a read */
 		rp_spi_xfer_data(0);
-}
 
-void __attribute__((naked))
-rp_flash_write_stub(const uint32_t command, const uint32_t dest, const uint32_t *const src, const uint32_t length)
-{
-	rp_spi_setup_xfer(command, dest, length);
-
+	/* Now write out the data requested */
 	for (size_t i = 0; i < length; ++i)
+		/* Do a write to read*/
 		rp_spi_xfer_data(src[i]);
 
+	/* Deselect the Flash to complete the transaction */
 	SPI_CHIP_SELECT(RP_GPIO_QSPI_CS_DRIVE_HIGH);
-
-	stub_exit(0);
+	stub_exit(1);
 }
