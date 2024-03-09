621474567a1cadba72c6249656174e714ae754b7
OmniTechnoMancer <OmniTechnoMancer@wah.quest>
2024-03-09 16:08:14 +0000

    gdb_main: Convert 'M' packet handler to use read_hex32
    Replace use of sscanf with read_hex32, follow character check is used
    to match ',' and ':' the rest pointer is used to calculate the remaining
    length of buffer for bounds checking the provided length is valid.

--- src/gdb_main.c
+++ src/gdb_main.c
@@ -166,20 +166,21 @@ int32_t gdb_main_loop(target_controller_s *tc, char *pbuf, size_t pbuf_size, siz
 	case 'M': { /* 'M addr,len:XX': Write len bytes to addr */
 		uint32_t addr = 0;
 		uint32_t len = 0;
-		int hex;
 		ERROR_IF_NO_TARGET();
-		sscanf(pbuf, "M%" SCNx32 ",%" SCNx32 ":%n", &addr, &len, &hex);
-		if (len > (unsigned)(size - hex) / 2U) {
-			gdb_putpacketz("E02");
-			break;
-		}
-		DEBUG_GDB("M packet: addr = %" PRIx32 ", len = %" PRIx32 "\n", addr, len);
-		uint8_t *mem = alloca(len);
-		unhexify(mem, pbuf + hex, len);
-		if (target_mem_write(cur_target, addr, mem, len))
-			gdb_putpacketz("E01");
-		else
-			gdb_putpacketz("OK");
+		if (read_hex32(pbuf + 1, &rest, &addr, ',') && read_hex32(rest, &rest, &len, ':')) {
+			if (len > (size - (size_t)(pbuf - rest)) / 2U) {
+				gdb_putpacketz("E02");
+				break;
+			}
+			DEBUG_GDB("M packet: addr = %" PRIx32 ", len = %" PRIx32 "\n", addr, len);
+			uint8_t *mem = alloca(len);
+			unhexify(mem, rest, len);
+			if (target_mem_write(cur_target, addr, mem, len))
+				gdb_putpacketz("E01");
+			else
+				gdb_putpacketz("OK");
+		} else
+			gdb_putpacketz("EFF");
 		break;
 	}
 	/*
