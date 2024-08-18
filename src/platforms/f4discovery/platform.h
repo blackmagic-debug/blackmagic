c694e4a15a5c8af1b31bb862af6622b3d2db6650
ALTracer <11005378+ALTracer@users.noreply.github.com>
2024-08-18 15:45:31 +0300

    f4discovery: Update TraceSWO TIM macros

--- src/platforms/f4discovery/platform.h
+++ src/platforms/f4discovery/platform.h
@@ -130,6 +130,9 @@
 #define TRACE_TIM_CLK_EN() rcc_periph_clock_enable(RCC_TIM3)
 #define TRACE_IRQ          NVIC_TIM3_IRQ
 #define TRACE_ISR(x)       tim3_isr(x)
+#define TRACE_IC_IN        TIM_IC_IN_TI1
+#define TRACE_TRIG_IN      TIM_SMCR_TS_TI1FP1
+#define TRACE_TIM_PIN_AF   GPIO_AF2
 
 #define SET_RUN_STATE(state)      \
 	{                             \
