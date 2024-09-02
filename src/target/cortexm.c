7efeebf9914b1fa3e11ebf4e6165c306076fe877
dragonmux <git@dragonmux.network>
2024-09-03 00:45:29 +0100

    cortexm: Cleaned up the top of function comment for `cortexm_build_target_description()`

--- src/target/cortexm.c
+++ src/target/cortexm.c
@@ -1212,36 +1212,38 @@ static size_t cortexm_build_target_fpu_description(char *const buffer, const siz
  * function will return the amount of bytes that would be necessary to create this string.
  *
  * The string it creates is XML-equivalent to the following:
- * "<?xml version=\"1.0\"?>"
- * "<!DOCTYPE target SYSTEM \"gdb-target.dtd\">"
- * "<target>"
- * "  <architecture>arm</architecture>"
- * "  <feature name=\"org.gnu.gdb.arm.m-profile\">"
- * "    <reg name=\"r0\" bitsize=\"32\"/>"
- * "    <reg name=\"r1\" bitsize=\"32\"/>"
- * "    <reg name=\"r2\" bitsize=\"32\"/>"
- * "    <reg name=\"r3\" bitsize=\"32\"/>"
- * "    <reg name=\"r4\" bitsize=\"32\"/>"
- * "    <reg name=\"r5\" bitsize=\"32\"/>"
- * "    <reg name=\"r6\" bitsize=\"32\"/>"
- * "    <reg name=\"r7\" bitsize=\"32\"/>"
- * "    <reg name=\"r8\" bitsize=\"32\"/>"
- * "    <reg name=\"r9\" bitsize=\"32\"/>"
- * "    <reg name=\"r10\" bitsize=\"32\"/>"
- * "    <reg name=\"r11\" bitsize=\"32\"/>"
- * "    <reg name=\"r12\" bitsize=\"32\"/>"
- * "    <reg name=\"sp\" bitsize=\"32\" type=\"data_ptr\"/>"
- * "    <reg name=\"lr\" bitsize=\"32\" type=\"code_ptr\"/>"
- * "    <reg name=\"pc\" bitsize=\"32\" type=\"code_ptr\"/>"
- * "    <reg name=\"xpsr\" bitsize=\"32\"/>"
- * "    <reg name=\"msp\" bitsize=\"32\" save-restore=\"no\" type=\"data_ptr\"/>"
- * "    <reg name=\"psp\" bitsize=\"32\" save-restore=\"no\" type=\"data_ptr\"/>"
- * "    <reg name=\"primask\" bitsize=\"8\" save-restore=\"no\"/>"
- * "    <reg name=\"basepri\" bitsize=\"8\" save-restore=\"no\"/>"
- * "    <reg name=\"faultmask\" bitsize=\"8\" save-restore=\"no\"/>"
- * "    <reg name=\"control\" bitsize=\"8\" save-restore=\"no\"/>"
- * "  </feature>"
- * "</target>"
+ *  <?xml version=\"1.0\"?>
+ *  <!DOCTYPE target SYSTEM \"gdb-target.dtd\">
+ *  <target>
+ *      <architecture>arm</architecture>
+ *      <feature name="org.gnu.gdb.arm.m-profile">
+ *          <reg name="r0" bitsize="32"/>
+ *          <reg name="r1" bitsize="32"/>
+ *          <reg name="r2" bitsize="32"/>
+ *          <reg name="r3" bitsize="32"/>
+ *          <reg name="r4" bitsize="32"/>
+ *          <reg name="r5" bitsize="32"/>
+ *          <reg name="r6" bitsize="32"/>
+ *          <reg name="r7" bitsize="32"/>
+ *          <reg name="r8" bitsize="32"/>
+ *          <reg name="r9" bitsize="32"/>
+ *          <reg name="r10" bitsize="32"/>
+ *          <reg name="r11" bitsize="32"/>
+ *          <reg name="r12" bitsize="32"/>
+ *          <reg name="sp" bitsize="32" type="data_ptr"/>
+ *          <reg name="lr" bitsize="32" type="code_ptr"/>
+ *          <reg name="pc" bitsize="32" type="code_ptr"/>
+ *          <reg name="xpsr" bitsize="32" regnum="25"/>
+ *      </feature>
+ *      <feature name="org.gnu.gdb.arm.m-system">
+ *          <reg name="msp" bitsize="32" save-restore="no" type="data_ptr"/>
+ *          <reg name="psp" bitsize="32" save-restore="no" type="data_ptr"/>
+ *          <reg name="primask" bitsize="8" save-restore="no"/>
+ *          <reg name="basepri" bitsize="8" save-restore="no"/>
+ *          <reg name="faultmask" bitsize="8" save-restore="no"/>
+ *          <reg name="control" bitsize="8" save-restore="no"/>
+ *      </feature>
+ *  </target>
  */
 static size_t cortexm_build_target_description(
 	char *const buffer, const size_t max_length, const uint32_t target_options)
