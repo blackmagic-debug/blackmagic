bad22c48d65aeffe81be9b4075593045fb104424
Sid Price <sid@sidprice.com>
2024-10-05 16:51:33 -0600

    ctxlink/misc: Updates to copyright on ctsLink project files.

--- src/platforms/ctxlink/ctxLink_mode_led.c
+++ src/platforms/ctxlink/ctxLink_mode_led.c
@@ -1,8 +1,22 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// file:	src\platforms\ctxLink\wdbp_mode_led.c
-//
-// summary:	Wdbp mode LED class, provides support for indicating the mode of ctxLink using a single LED
-////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * This file is part of the Black Magic Debug project.
+ *
+ * Copyright (C) 2024 Sid Price <sid@sidprice.com>
+ * Written by Sid Price <sid@sidprice.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "general.h"
 #include "platform.h"
