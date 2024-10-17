270acba24a80d01e50577f68c78a8ab88ab5b211
Sid Price <sid@sidprice.com>
2024-10-17 13:55:10 -0600

    ctxlink/misc: Updates to copyright on ctsLink project files.

--- src/platforms/ctxlink/WiFi_Server.h
+++ src/platforms/ctxlink/WiFi_Server.h
@@ -1,4 +1,25 @@
-#pragma once
+/*
+ * This file is part of the Black Magic Debug project.
+ *
+ * Copyright (C) 2024 Sid Price <sid@sidprice.com>
+ * Written by Sid Price <sid@sidprice.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef WIFI_SERVER_H
+#define WIFI_SERVER_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,7 +32,7 @@ bool is_gdb_client_connected(void);
 
 void data_tcp_server(void);
 bool is_uart_client_connected(void);
-void send_uart_data(uint8_t *lpBuffer, uint8_t length);
+void send_uart_data(uint8_t *buffer, uint8_t length);
 
 bool swo_trace_server_active(void);
 void wifi_setup_swo_trace_server(void);
@@ -25,3 +46,4 @@ unsigned char wifi_get_next_to(uint32_t timeout);
 #ifdef __cplusplus
 }
 #endif /*  __cplusplus */
+#endif // WIFI_SERVER_H
