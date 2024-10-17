b0d41c26452ed6d19a49f9a5fec02b4ce49867e2
Sid Price <sid@sidprice.com>
2024-10-17 13:55:10 -0600

    ctxlink/platform: Enable SWO features

--- src/platforms/ctxlink/platform.h
+++ src/platforms/ctxlink/platform.h
@@ -29,7 +29,7 @@
 #include "timing.h"
 #include "timing_stm32.h"
 
-// #define PLATFORM_HAS_TRACESWO
+#define PLATFORM_HAS_TRACESWO
 #define PLATFORM_HAS_POWER_SWITCH
 
 #define PLATFORM_IDENT "(ctxLink) "
