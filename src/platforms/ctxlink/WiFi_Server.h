819bd9c4408f732540f85b0e6b6050ac986b2f75
Sid Price <sid@sidprice.com>
2024-10-26 16:29:30 -0600

    ctxlink/wifi_server: Run clang format

--- src/platforms/ctxlink/WiFi_Server.h
+++ src/platforms/ctxlink/WiFi_Server.h
@@ -3,26 +3,25 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-	void APP_Initialize(void);
-	void APP_Task(void);
-	//
-	void GDB_TCPServer(void);
-	bool isGDBClientConnected(void);
+void APP_Initialize(void);
+void APP_Task(void);
+//
+void GDB_TCPServer(void);
+bool isGDBClientConnected(void);
 
-	void DATA_TCPServer (void);
-	bool isUARTClientConnected(void) ;
-	void SendUartData(uint8_t *lpBuffer, uint8_t length) ;
+void DATA_TCPServer(void);
+bool isUARTClientConnected(void);
+void SendUartData(uint8_t *lpBuffer, uint8_t length);
 
-	bool swoTraceServerActive(void) ;
-	void WiFi_setupSwoTraceServer(void) ;
-	bool isSwoTraceClientConnected(void) ;
-	void SendSwoTraceData(uint8_t *lpBuffer, uint8_t length);
+bool swoTraceServerActive(void);
+void WiFi_setupSwoTraceServer(void);
+bool isSwoTraceClientConnected(void);
+void SendSwoTraceData(uint8_t *lpBuffer, uint8_t length);
 
-	void WiFi_gdb_putchar(unsigned char c, int flush);
-	bool WiFi_GotClient( void );
-	unsigned char WiFi_GetNext(void);
-	unsigned char WiFi_GetNext_to(uint32_t timeout);	
-	#ifdef  __cplusplus
+void WiFi_gdb_putchar(unsigned char c, int flush);
+bool WiFi_GotClient(void);
+unsigned char WiFi_GetNext(void);
+unsigned char WiFi_GetNext_to(uint32_t timeout);
+#ifdef __cplusplus
 }
 #endif /*  __cplusplus */
-
