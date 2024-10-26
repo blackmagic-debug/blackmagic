f0602ac7513ba530a81e81baa3d23aa0fa4843b7
Sid Price <sid@sidprice.com>
2024-10-26 16:29:54 -0600

    ctxlink/ctxlink_mode_led: Address header file include guard and license.

--- src/platforms/ctxlink/ctxLink_mode_led.h
+++ src/platforms/ctxlink/ctxLink_mode_led.h
@@ -1,10 +1,26 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// file:	wdbp_mode_led.h
-//
-// summary:	Declares the ctxLink mode_LED class
-////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * This file is part of the Black Magic Debug project.
+ *
+ * Copyright (C) 2024 Sid Price <sid@sidprice.com>
+ * Written by Sid Price <sid@sidprice.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
-#pragma once
+#ifndef WDP_MODE_LED_H
+#define WDP_MODE_LED_H
 
 typedef enum {
 	mode_led_idle_state = 0,  //	 0
@@ -30,3 +46,5 @@ typedef enum {
 extern mode_led_modes_e led_mode;
 
 void mode_led_task(void);
+
+#endif // WDP_MODE_LED_H
