66eb6cc518e41c8683b1700415da5da443ba284c
dragonmux <git@dragonmux.network>
2025-01-07 20:55:34 +0000

    common/usb_descriptors: Bumped the device version number

--- src/platforms/common/usb_descriptors.h
+++ src/platforms/common/usb_descriptors.h
@@ -50,7 +50,7 @@ static const usb_device_descriptor_s dev_desc = {
 #endif
 	.idVendor = 0x1d50,
 	.idProduct = 0x6018,
-	.bcdDevice = 0x0109,
+	.bcdDevice = 0x0200,
 	.iManufacturer = 1,
 	.iProduct = 2,
 	.iSerialNumber = 3,
