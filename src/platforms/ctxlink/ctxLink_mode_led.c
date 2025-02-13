0bf3444b81a68236d487958d91cb248f0be5ec6c
Sid Price <sid@sidprice.com>
2025-02-13 11:52:56 -0700

    ctxlink/ctxlink_mode_led: Fix enum value.

--- src/platforms/ctxlink/ctxLink_mode_led.c
+++ src/platforms/ctxlink/ctxLink_mode_led.c
@@ -108,8 +108,8 @@ void mode_led_task(void)
 			 */
 		if (led_mode != mode_led_idle) {
 			/*
-				 * Set up the led control registers according to the requested mode
-				 */
+			 * Set up the led control registers according to the requested mode
+			 */
 			mode_task_state = mode_led_state_on;
 #ifndef INSTRUMENT
 			gpio_set(LED_PORT, LED_MODE);
