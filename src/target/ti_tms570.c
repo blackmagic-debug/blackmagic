ccb91223b060018b21ead67afb876262939d0a26
Sean Cross <sean@xobs.io>
2025-04-02 18:23:24 +0800

    tms570: swap check word since this target is BE
    Since we don't swap everything now, the hardcoded check value needs to be
    swapped in order to properly read the value.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/target/ti_tms570.c
+++ src/target/ti_tms570.c
@@ -5,8 +5,8 @@
 
 #define TMS570_SYS_BASE            0xffffff00U
 #define TMS570_SYS_DEVID           (TMS570_SYS_BASE + 0xf0U)
-#define TMS570_SCM_DEVID_ID_MASK   0x7ffe0007U
-#define TMS570_SCM_REVID_ID_TMS570 0x00440005U
+#define TMS570_SCM_DEVID_ID_MASK   0x0700fe7fU /* Note: value is swapped since target is BE */
+#define TMS570_SCM_REVID_ID_TMS570 0x05004400U /* Note: value is swapped since target is BE */
 
 /* Base address for the OCRAM regions, including their mirrors (including RETRAM) */
 #define TMS570_SRAM_BASE     0x08000000U
@@ -19,8 +19,10 @@ bool ti_tms570_probe(target_s *const target)
 {
 	const uint32_t part_id = target_mem32_read32(target, TMS570_SYS_DEVID);
 	if (!part_id || ((part_id & TMS570_SCM_DEVID_ID_MASK) != TMS570_SCM_REVID_ID_TMS570)) {
+		fprintf(stderr, "Part ID 0x%08x was unrecognized\n", part_id);
 		return false;
 	}
+	fprintf(stderr, "Part ID 0x%08x was recognized as a TMS570\n", part_id);
 
 	target->driver = "TMS570";
 	target_add_ram32(target, TMS570_SRAM_BASE, TMS570_SRAM_SIZE);
