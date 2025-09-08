1cc4f56d163be5f3bfebca6ad4d189a2c0ea8b87
dragonmux <git@dragonmux.network>
2025-09-08 04:54:51 +0100

    nrf91: Normalised the code organisation a bit

--- src/target/nrf91.c
+++ src/target/nrf91.c
@@ -15,6 +15,49 @@
 #define NRF91_NVMC_CONFIG_EEN  0x2U // Erase enable
 #define NRF91_NVMC_CONFIG_PEEN 0x3U // Partial erase enable
 
+#define ID_NRF91 0x0090U
+
+static bool nrf91_flash_erase(target_flash_s *flash, target_addr_t addr, size_t len);
+static bool nrf91_flash_write(target_flash_s *flash, target_addr_t dest, const void *src, size_t len);
+
+static void nrf91_add_flash(target_s *target, uint32_t addr, size_t length, size_t erasesize)
+{
+	target_flash_s *flash = calloc(1, sizeof(*flash));
+	if (!flash) { /* calloc failed: heap exhaustion */
+		DEBUG_WARN("calloc: failed in %s\n", __func__);
+		return;
+	}
+
+	flash->start = addr;
+	flash->length = length;
+	flash->blocksize = erasesize;
+	flash->erase = nrf91_flash_erase;
+	flash->write = nrf91_flash_write;
+	flash->erased = 0xff;
+	target_add_flash(target, flash);
+}
+
+bool nrf91_probe(target_s *target)
+{
+	adiv5_access_port_s *ap = cortex_ap(target);
+
+	if (ap->dp->version < 2U)
+		return false;
+
+	switch (ap->dp->target_partno) {
+	case ID_NRF91:
+		target->driver = "nRF9160";
+		target->target_options |= TOPT_INHIBIT_NRST;
+		target_add_ram32(target, 0x20000000, 256U * 1024U);
+		nrf91_add_flash(target, 0, 4096U * 256U, 4096U);
+		break;
+	default:
+		return false;
+	}
+
+	return true;
+}
+
 static bool nrf91_wait_ready(target_s *const target, platform_timeout_s *const timeout)
 {
 	/* Poll for NVMC_READY */
@@ -63,43 +106,5 @@ static bool nrf91_flash_write(target_flash_s *flash, target_addr_t dest, const v
 		return false;
 	/* Return to read-only */
 	target_mem32_write32(target, NRF91_NVMC_CONFIG, NRF91_NVMC_CONFIG_REN);
-	return true;
-}
-
-static void nrf91_add_flash(target_s *target, uint32_t addr, size_t length, size_t erasesize)
-{
-	target_flash_s *flash = calloc(1, sizeof(*flash));
-	if (!flash) { /* calloc failed: heap exhaustion */
-		DEBUG_WARN("calloc: failed in %s\n", __func__);
-		return;
-	}
-
-	flash->start = addr;
-	flash->length = length;
-	flash->blocksize = erasesize;
-	flash->erase = nrf91_flash_erase;
-	flash->write = nrf91_flash_write;
-	flash->erased = 0xff;
-	target_add_flash(target, flash);
-}
-
-bool nrf91_probe(target_s *target)
-{
-	adiv5_access_port_s *ap = cortex_ap(target);
-
-	if (ap->dp->version < 2U)
-		return false;
-
-	switch (ap->dp->target_partno) {
-	case 0x90:
-		target->driver = "nRF9160";
-		target->target_options |= TOPT_INHIBIT_NRST;
-		target_add_ram32(target, 0x20000000, 256U * 1024U);
-		nrf91_add_flash(target, 0, 4096U * 256U, 4096U);
-		break;
-	default:
-		return false;
-	}
-
 	return true;
 }
