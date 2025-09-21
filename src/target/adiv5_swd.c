245683dcc17c2902da8cfe3ca2989d7b3c90e5c5
dragonmux <git@dragonmux.network>
2025-09-21 01:27:36 +0100

    adiv5_swd: Fixed a consistency issue in one of the mode switch diagnostics

--- src/target/adiv5_swd.c
+++ src/target/adiv5_swd.c
@@ -87,7 +87,7 @@ static void dormant_to_swd_sequence(void)
 	swd_proc.seq_out(ADIV5_JTAG_TO_DORMANT_SEQUENCE1, 31U);
 	swd_proc.seq_out(ADIV5_JTAG_TO_DORMANT_SEQUENCE2, 8U);
 	/* Send the 128-bit Selection Alert sequence on SWDIOTMS */
-	DEBUG_INFO("Switching out of dormant state into SWD\n");
+	DEBUG_INFO("Switching from dormant to SWD\n");
 	swd_proc.seq_out(ADIV5_SELECTION_ALERT_SEQUENCE_0, 32U);
 	swd_proc.seq_out(ADIV5_SELECTION_ALERT_SEQUENCE_1, 32U);
 	swd_proc.seq_out(ADIV5_SELECTION_ALERT_SEQUENCE_2, 32U);
