21ce9ac0707d34cecb8573d8d320e3464b9529bb
dragonmux <git@dragonmux.network>
2025-12-22 07:57:55 +0000

    bmp-v3: Initialise the LED pins so we can see the GDB machinary state

--- src/platforms/bmp-v3/platform.h
+++ src/platforms/bmp-v3/platform.h
@@ -267,6 +267,6 @@ extern int hwversion;
 
 #define SET_RUN_STATE(state)   running_status = (state)
 #define SET_IDLE_STATE(state)  gpio_set_val(LED_IDLE_RUN_PORT, LED_IDLE_RUN_PIN, state)
-#define SET_ERROR_STATE(state) gpio_set_val(LED_ERROR_PORT, LED_ERROR_PIN, state)
+#define SET_ERROR_STATE(state) gpio_set_val(LED_ERROR_PORT, LED_ERROR_PIN, !state)
 
 #endif /* PLATFORMS_BMP_V3_PLATFORM_H */
