bd37f13022dc1f35b299014fc6ee44853332bc13
ALTracer <11005378+ALTracer@users.noreply.github.com>
2026-03-05 21:03:05 +0300

    stm32f1: Hardcode GD32VW553 memory map

--- src/target/stm32f1.c
+++ src/target/stm32f1.c
@@ -398,9 +398,10 @@ static void gd32vf1_detach(target_s *const target)
 bool gd32vw5_probe(target_s *const target)
 {
 	const uint16_t device_id = target_mem32_read32(target, GD32E5_DBGMCU_BASE) & 0xfffU;
-	const uint32_t signature = target_mem32_read32(target, GD32Fx_FLASHSIZE);
-	const uint16_t flash_size = signature & 0xffffU;
-	const uint16_t ram_size = signature >> 16U;
+	/* Either 2 or 4 MiB of main SiP Flash */
+	const uint16_t flash_size = 4096U;
+	/* SRAM0/1/2 each 64 KiB, SRAM3 128 KiB (96+32 shared) */
+	const uint16_t ram_size = 320U;
 	DEBUG_WARN("Stub for detection of GD32VW553. DBG_ID=0x%x, RAM=%u, flash=%u\n", device_id, ram_size, flash_size);
 	target->driver = "GD32VW5";
 	target->part_id = device_id;
