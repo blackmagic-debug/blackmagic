a523f16a110d14fe60a77f79cc43ab13e51f12b5
ALTracer <altracer@noreply.codeberg.org>
2026-04-12 18:40:05 +0300

    riscv32: Compose the progbuf read/write instructions via macros, drop arrays

--- src/target/riscv32.c
+++ src/target/riscv32.c
@@ -63,6 +63,27 @@
 #define RV32_MATCH_BEFORE 0x00000000U
 #define RV32_MATCH_AFTER  0x00040000U
 
+/* Uncompressed instruction encodings for progbuf-based memory access */
+#define RV_LOAD_BYTE  0x00000003U
+#define RV_LOAD_HALF  0x00001003U
+#define RV_LOAD_WORD  0x00002003U
+#define RV_STORE_BYTE 0x00000023U
+#define RV_STORE_HALF 0x00001023U
+#define RV_STORE_WORD 0x00002023U
+/* Only GPR A0 & A1 are used for source/destination (and saved-restored) */
+#define RV_RS1_A0 0x00050000U
+#define RV_RS2_A1 0x00b00000U
+#define RV_RD_A0  0x00000500U
+#define RV_RD_A1  0x00000580U
+/* RV32I opcodes: load word/half/byte from address pointed-by a0 into a1 */
+#define RV_LW_A1_A0 (RV_LOAD_WORD | RV_RD_A1 | RV_RS1_A0) // 0x00052583U // lw a1, 0(a0)
+#define RV_LH_A1_A0 (RV_LOAD_HALF | RV_RD_A1 | RV_RS1_A0) // 0x00051583U // lh a1, 0(a0)
+#define RV_LB_A1_A0 (RV_LOAD_BYTE | RV_RD_A1 | RV_RS1_A0) // 0x00050583U // lb a1, 0(a0)
+/* RV32I opcodes: store word/half/byte from a1 into address pointed-by a0 */
+#define RV_SW_A1_A0 (RV_STORE_WORD | RV_RS2_A1 | RV_RS1_A0) // 0x00b52023U // sw a1, 0(a0)
+#define RV_SH_A1_A0 (RV_STORE_HALF | RV_RS2_A1 | RV_RS1_A0) // 0x00b51023U // sh a1, 0(a0)
+#define RV_SB_A1_A0 (RV_STORE_BYTE | RV_RS2_A1 | RV_RS1_A0) // 0x00b50023U // sb a1, 0(a0)
+
 static size_t riscv32_reg_read(target_s *target, uint32_t reg, void *data, size_t max);
 static size_t riscv32_reg_write(target_s *target, uint32_t reg, const void *data, size_t max);
 static void riscv32_regs_read(target_s *target, void *data);
@@ -585,35 +606,22 @@ static void riscv32_progbuf_mem_read(
 	const uint8_t access_width = riscv_mem_access_width(hart, src, len);
 	const uint8_t access_length = 1U << access_width;
 
-	/* RV32I opcodes: load word/half/byte from address A0 into A1 (clobber), postincrement A0 */
-	static const uint32_t progbuf_read32[2] = {
-		0x00052583U, // lw a1, 0(a0)
-		0x00450513U, // addi a0, a0, 4
-	};
-	static const uint32_t progbuf_read16[2] = {
-		0x00051583U, // lh a1, 0(a0)
-		0x00450513U, // addi a0, a0, 4
-	};
-	static const uint32_t progbuf_read8[2] = {
-		0x00050583U, // lb a1, 0(a0)
-		0x00450513U, // addi a0, a0, 4
-	};
-	const uint32_t *progbuf_read = progbuf_read32;
+	uint32_t progbuf_read = RV_LOAD_BYTE;
 	switch (access_width) {
 	case RV_MEM_ACCESS_8_BIT:
-		progbuf_read = progbuf_read8;
+		progbuf_read = RV_LB_A1_A0;
 		break;
 	case RV_MEM_ACCESS_16_BIT:
-		progbuf_read = progbuf_read16;
+		progbuf_read = RV_LH_A1_A0;
 		break;
 	case RV_MEM_ACCESS_32_BIT:
-		progbuf_read = progbuf_read32;
+		progbuf_read = RV_LW_A1_A0;
 		break;
 	default:
 		return;
 	}
 	/* Fill the program buffer */
-	if (!riscv_dm_write(hart->dbg_module, RV_DM_PROGBUF_BASE, progbuf_read[0]))
+	if (!riscv_dm_write(hart->dbg_module, RV_DM_PROGBUF_BASE, progbuf_read))
 		return;
 	/* Append literal ebreak (if impebreak is not reached) */
 	if (hart->progbuf_size > 1U) {
@@ -656,35 +664,23 @@ static void riscv32_progbuf_mem_write(
 	/* Figure out the maxmial width of access to perform, up to the bitness of the target */
 	const uint8_t access_width = riscv_mem_access_width(hart, dest, len);
 	const uint8_t access_length = 1U << access_width;
-	/* RV32I opcodes: store word/half/byte in a1 into address pointed-by a0 */
-	static const uint32_t progbuf_write32[] = {
-		0x00b52023U, // sw a1, 0(a0)
-		0x00450513U, // addi a0, a0, 4
-	};
-	static const uint32_t progbuf_write16[] = {
-		0x00b51023U, // sh a1, 0(a0)
-		0x00450513U, // addi a0, a0, 4
-	};
-	static const uint32_t progbuf_write8[] = {
-		0x00b50023U, // sb a1, 0(a0)
-		0x00450513U, // addi a0, a0, 4
-	};
-	const uint32_t *progbuf_write = progbuf_write32;
+
+	uint32_t progbuf_write = RV_STORE_BYTE;
 	switch (access_width) {
 	case RV_MEM_ACCESS_8_BIT:
-		progbuf_write = progbuf_write8;
+		progbuf_write = RV_SB_A1_A0;
 		break;
 	case RV_MEM_ACCESS_16_BIT:
-		progbuf_write = progbuf_write16;
+		progbuf_write = RV_SH_A1_A0;
 		break;
 	case RV_MEM_ACCESS_32_BIT:
-		progbuf_write = progbuf_write32;
+		progbuf_write = RV_SW_A1_A0;
 		break;
 	default:
 		return;
 	}
 	/* Fill the program buffer */
-	if (!riscv_dm_write(hart->dbg_module, RV_DM_PROGBUF_BASE, progbuf_write[0]))
+	if (!riscv_dm_write(hart->dbg_module, RV_DM_PROGBUF_BASE, progbuf_write))
 		return;
 	/* Append literal ebreak (if impebreak is not reached) */
 	if (hart->progbuf_size > 1U) {
